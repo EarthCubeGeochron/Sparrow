a907cdeb16b5a1034f1fafaa84814ba45bd1fc84
Daven Quinn <dev@davenquinn.com>
2018-10-04 15:41:48 -0500

    Added script files

---
+++ labdata/util.py
@@ -0,0 +1,19 @@
+from pandas import read_sql
+
+def run_query(db, filename_or_query, **kwargs):
+    """
+    Run a query on a SQL database (represented by
+    a SQLAlchemy database object) and turn it into a
+    `Pandas` dataframe.
+    """
+
+    if "SELECT" in str(filename_or_query):
+        # We are working with a query string instead of
+        # an SQL file.
+        sql = filename_or_query
+    else:
+        with open(filename_or_query) as f:
+            sql = f.read()
+
+    return read_sql(sql,db,**kwargs)
+
---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(
+    name='labdata',
+    version='0.1',
+    package_dir={'labdata': 'labdata'},
+    install_requires=['sqlalchemy', 'pandas'])
