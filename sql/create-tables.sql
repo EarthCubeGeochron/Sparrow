a9450fd6a7dccc888cf6e16f53cace04abc55bbd
Daven Quinn <dev@davenquinn.com>
2018-11-16 05:50:41 -0600

    updated tables to normalize

--- sql/create-tables.sql
+++ sql/create-tables.sql
@@ -34,13 +34,21 @@ Vocabularies
 Tables to integrate units, etc.
 from curated collections
 */
-DROP SCHEMA vocabulary CASCADE;
+--DROP SCHEMA vocabulary CASCADE;
+DROP TABLE vocabulary.material;
 CREATE SCHEMA vocabulary;
 
 CREATE TABLE vocabulary.parameter (
   id text PRIMARY KEY,
   description text,
   authority text
+);
+
+CREATE TABLE vocabulary.material (
+  id text PRIMARY KEY,
+  description text,
+  authority text,
+  type_of text REFERENCES vocabulary.material(id)
 );
 
 CREATE TABLE vocabulary.method (
@@ -52,8 +60,16 @@ CREATE TABLE vocabulary.method (
 
 CREATE TABLE vocabulary.unit (
   id text PRIMARY KEY,
+  description text,
   authority text
 );
+
+CREATE TABLE vocabulary.error_metric (
+  id text PRIMARY KEY,
+  description text,
+  authority text
+);
+
 
 -- Projects
 
@@ -88,16 +104,28 @@ CREATE TABLE datum_type (
   parameter text REFERENCES vocabulary.parameter(id) NOT NULL,
   unit text REFERENCES vocabulary.unit(id) NOT NULL,
   error_unit text REFERENCES vocabulary.unit(id),
+  error_metric text REFERENCES vocabulary.error_metric(id),
   is_computed boolean, -- Can be rebuilt from data IN THE DATABASE
-  is_interpreted boolean -- Results from a data-reduction process
+  is_interpreted boolean, -- Results from a data-reduction process
+  description text,
+  UNIQUE (parameter, unit, error_unit,
+          error_metric, is_computed, is_interpreted)
 );
 
--- Samples
-
 CREATE TABLE sample (
+  /*
+  Sample: an object to be measured
+
+  Potential issues:
+  - Samples potentially have several levels of abstraction
+    (e.g. different replicates or drillings from the same rock sample)
+    (could have a self-referential 'part_of' relation on sample...)
+  - Samples might belong to several projects
+  */
   id text PRIMARY KEY,
   igsn text UNIQUE,
   project_id text REFERENCES project(id),
+  material text REFERENCES vocabulary.material(id),
   location geometry
 );
 
@@ -110,7 +138,9 @@ CREATE TABLE session (
   date timestamptz NOT NULL,
   end_date timestamptz,
   instrument integer REFERENCES instrument(id),
-  technique text REFERENCES vocabulary.method(id)
+  technique text REFERENCES vocabulary.method(id),
+  target text REFERENCES vocabulary.material(id),
+  UNIQUE (sample_id, date, instrument, technique)
 );
 
 /*
@@ -118,14 +148,18 @@ SINGLE ANALYSES
 These two tables will end up needing data-type specific columns
 */
 CREATE TABLE analysis (
-  -- Set of data measured together at one time on one instrument
-  -- Ex: different oxides measured on an EPMA are *data* in a single analysis
+  /*
+  Set of data measured together at one time on one instrument
+  Example: different oxides measured on an EPMA
+           are *data* in a single analysis
+  */
   id serial PRIMARY KEY,
   session_id integer REFERENCES session(id) NOT NULL,
   session_index integer, -- captures ordering within a session
-  date timestamptz
-  /* The analysis must be related to a sample
-     whether or not sessions are defined */
+  date timestamptz,
+  material text REFERENCES vocabulary.material(id)
+  /* Not really sure that material is the best parameterization
+     of this concept... */
 );
 
 CREATE TABLE datum (
@@ -134,5 +168,6 @@ CREATE TABLE datum (
   type integer REFERENCES datum_type(id),
   value numeric NOT NULL,
   error numeric,
-  is_bad boolean
+  is_bad boolean,
+  UNIQUE (analysis, type)
 );
--- sql/normalize-measurements.sql
+++ sql/normalize-measurements.sql
@@ -29,14 +29,30 @@ SELECT DISTINCT ON (sample)
   'zebra-nappe'
 FROM test_data.detrital_zircon;
 
+-- Materials
+INSERT INTO vocabulary.material (id, description, type_of)
+VALUES
+('rock', 'Rock', null),
+('mineral','Mineral', null),
+('xenolith', 'Xenolith', 'rock'),
+('sp-xeno','Spinel peridotite xenolith', 'xenolith'),
+('basalt', 'Basalt', 'rock'),
+('pyx', 'Pyroxene', 'mineral'),
+('cpx', 'Clinopyroxene', 'pyx'),
+('opx', 'Orthopyroxene', 'pyx');
+
 WITH s AS (
   SELECT sample_id FROM test_data.probe_session
   UNION ALL
   SELECT sample_id FROM test_data.sims_measurement
 )
-INSERT INTO sample (id, project_id)
+INSERT INTO sample (id, material, project_id)
 SELECT DISTINCT ON (sample_id)
   sample_id,
+  CASE WHEN sample_id = 'CK-2'
+    THEN 'basalt'
+    ELSE 'sp-xeno'
+  END,
   'crystal-knob'
 FROM s;
 
@@ -44,3 +60,139 @@ INSERT INTO instrument (name, description) VALUES
 ('IMS-7fGEO', 'SIMS for rock geochemical analysis'),
 ('Element2', 'LA-ICP mass spectrometer'),
 ('JEOL JXA-8200', 'Electron probe microanalyzer');
+
+COMMIT;
+/*
+We'd have to go back to the raw data to recover the real
+sessions, since our project-specific database doesn't include
+this info.
+*/
+INSERT INTO session (sample_id, date, technique, instrument, target)
+SELECT
+	sample_id,
+	'2013-06-05' date,
+	'SIMS' technique,
+	(SELECT id FROM instrument WHERE name = 'IMS-7fGEO') instrument,
+  'sp-xeno'
+FROM test_data.sims_measurement
+GROUP BY (sample_id);
+
+INSERT INTO vocabulary.unit (id, description)
+VALUES
+('ppm_chondrite', 'atomic ppm normalized to Workman and Hart chondrite abundances'),
+('ppm (atomic)', 'atomic ppm (raw values)') -- Overlaps with EarthChem definition
+ON CONFLICT DO NOTHING;
+
+INSERT INTO vocabulary.error_metric (id, description)
+VALUES
+('std', 'Standard deviation')
+ON CONFLICT DO NOTHING;
+
+WITH a AS (
+SELECT
+	id orig_id,
+	(SELECT id FROM session WHERE sample_id = s.sample_id) session_id,
+	mineral,
+	name
+FROM test_data.sims_measurement s
+),
+b AS (
+SELECT
+orig_id,
+session_id,
+row_number() OVER(
+	PARTITION BY session_id
+	ORDER BY mineral, name
+) session_index,
+mineral material
+FROM a
+),
+c AS (
+INSERT INTO analysis (session_id, session_index, material)
+SELECT session_id, session_index, material
+FROM b
+RETURNING id, session_id, session_index
+),
+index AS (
+SELECT
+	orig_id,
+	c.session_id,
+	c.session_index,
+	id new_id
+FROM c
+JOIN b
+  ON c.session_id = b.session_id
+  AND c.session_index = b.session_index
+),
+__step5 AS (
+SELECT
+  new_id analysis_id,
+  (SELECT symbol FROM vocabulary.element WHERE number = s.element) parameter,
+  norm_ppm,
+  norm_std,
+  raw_ppm,
+  raw_std,
+  bad
+FROM test_data.sims_datum s
+JOIN index ON s.measurement_id = index.orig_id
+),
+-- All data
+__all_data AS (
+SELECT
+analysis_id,
+parameter,
+norm_ppm datum,
+norm_std error,
+'ppm_chondrite' AS unit,
+'norm' AS type,
+true AS is_computed,
+bad is_bad
+FROM __step5
+UNION ALL
+SELECT
+analysis_id,
+parameter,
+raw_ppm datum,
+raw_std error,
+'ppm (atomic)' AS unit,
+'raw' AS type,
+false AS is_computed,
+bad is_bad
+FROM __step5
+),
+__setup_parameters AS (
+INSERT INTO vocabulary.parameter (id)
+SELECT DISTINCT ON (parameter)
+parameter
+FROM __all_data
+ON CONFLICT DO NOTHING
+),
+__insert_datum_type AS (
+INSERT INTO datum_type (parameter, unit, error_unit, error_metric, is_computed, is_interpreted)
+SELECT DISTINCT ON (parameter, type)
+parameter,
+unit,
+unit error_unit,
+'std',
+is_computed,
+false
+FROM __all_data
+ON CONFLICT (parameter, unit, error_unit, error_metric, is_computed, is_interpreted)
+DO UPDATE SET is_computed = EXCLUDED.is_computed
+RETURNING *
+)
+INSERT INTO datum (analysis, type, value, error, is_bad)
+SELECT
+	analysis_id,
+	t.id,
+	datum AS value,
+	error,
+	is_bad
+FROM __all_data v
+JOIN __insert_datum_type t
+  ON t.parameter = v.parameter
+ AND t.unit = v.unit
+ AND t.error_unit = v.unit
+ON CONFLICT DO NOTHING
+RETURNING *;
+
