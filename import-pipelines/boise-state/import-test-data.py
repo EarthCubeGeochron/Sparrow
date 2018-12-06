f67eef7e3767bca3b99cac31debaa8c0d5f3ca87
Daven Quinn <dev@davenquinn.com>
2018-12-06 02:49:54 -0600

    Some more boise state import stuff

--- import-pipelines/boise-state/import-test-data.py
+++ import-pipelines/boise-state/import-test-data.py
@@ -1,20 +1,28 @@
+"""
+Import script for *ET-Redux* xml-formatted data
+from Boise State Geochronology Laboratory
+"""
+
 import pandas as P
-from xml.etree import ElementTree as ET
+import xml.etree.cElementTree as ET
 from sys import argv
 from itertools import islice
 from io import StringIO
 
-def parse_xml(content):
-    try:
-        tree = ET.XML(content)
-    except ET.ParseError as err:
-        lineno, column = err.position
-        line = next(islice(StringIO(content), lineno))
-        caret = '{:=>{}}'.format('^', column)
-        err.msg = '{}\n{}\n{}'.format(err, line, caret)
-        raise
-    return tree
+def parse_xml(content, strip_namespaces=True):
+    if not strip_namespaces:
+        return ET.XML(content)
+
+    it = ET.iterparse(StringIO(content))
+    for _, el in it:
+        if '}' not in el.tag:
+            continue
+        el.tag = el.tag.split('}', 1)[1]  # strip all namespaces
+    return it.root
 
 with open(argv[1]) as f:
+    # Read in XML
     et = parse_xml(f.read())
-    import IPython; IPython.embed()
+
+for frac in et.iter('analysisFractions'):
+    print(frac.attrib)
