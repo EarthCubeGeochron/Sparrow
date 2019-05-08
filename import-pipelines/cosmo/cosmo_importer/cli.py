e6ae3abccd15676baef38845a0e26bfbe344ea39
Daven Quinn <dev@davenquinn.com>
2019-05-08 13:43:58 -0500

    Updated cosmo importer

--- import-pipelines/cosmo/cosmo_importer/cli.py
+++ import-pipelines/cosmo/cosmo_importer/cli.py
@@ -20,6 +20,15 @@ def cli(stop_on_error=False, verbose=False):
     path = Path(env)
     assert path.is_dir()
 
+    db = Database()
+
+    # Temporary
+    db.engine.execute("TRUNCATE TABLE datum CASCADE;")
+    db.engine.execute("TRUNCATE TABLE datum_type CASCADE;")
+    db.engine.execute("TRUNCATE TABLE analysis CASCADE;")
+    db.engine.execute("TRUNCATE TABLE session CASCADE;")
+    db.engine.execute("TRUNCATE TABLE sample CASCADE;")
+
     files = path.glob("**/*_simplified.xlsx")
     files = (i for i in files if not i.stem.startswith('~$'))
     iterfiles(import_datafile, files)
