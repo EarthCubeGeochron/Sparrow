98b58ff026e681ea834f4a7953c97e6e8583c9c6
Daven Quinn <dev@davenquinn.com>
2019-05-13 03:36:08 -0400

    Partial igsn importer

--- import-pipelines/boise-state/boise_state_importer/igsn_importer.py
+++ import-pipelines/boise-state/boise_state_importer/igsn_importer.py
@@ -1,4 +1,5 @@
 from sparrow.import_helpers import BaseImporter
+from json import dumps
 
 class IGSNImporter(BaseImporter):
     def import_data(self, all=True):
@@ -9,6 +10,29 @@ class IGSNImporter(BaseImporter):
     def import_igsn(self, model):
         data = model.data
         if not data: return
-        data = data['sample']
-        if 'status' in data: return
-        print(data)
+        s = data['sample']
+        # Data with 'status' set are currently embargoed
+        if 'status' in s: return
+        v = dumps(s, indent=4, sort_keys=True)
+        print(v)
+
+        # Could set location precision from number of
+        # decimal places in lat/lon string
+        try:
+            location = self.location(s['longitude'], s['latitude'])
+        except KeyError:
+            location = None
+
+        igsn = s['igsn']
+        name = s['name']
+        if not name:
+            name = igsn
+
+        v = self.sample(
+            igsn=igsn,
+            defaults=dict(
+                id=name,
+                location=location
+            ))
+
+        self.db.session.commit()
