b6ce5a05515a094cc58b92503fd598462b63a7c9
Daven Quinn <dev@davenquinn.com>
2019-05-13 08:43:18 -0400

    Updated header

--- docs/sparrow-docs/src/components/header.js
+++ docs/sparrow-docs/src/components/header.js
@@ -1,26 +1,23 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import { css, jsx } from '@emotion/core'
+
+const style = css`
+max-width 960px;
+`
 
 const Header = ({ siteTitle }) => (
-  <header
+  <header css={style}
     style={{
-      background: `rebeccapurple`,
       marginBottom: `1.45rem`,
     }}
   >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
+    <div>
       <h1 style={{ margin: 0 }}>
         <Link
           to="/"
           style={{
-            color: `white`,
             textDecoration: `none`,
           }}
         >
