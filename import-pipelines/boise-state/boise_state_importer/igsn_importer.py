b23ac534bfb2bdd138191db57b4a3823252529e0
Daven Quinn <dev@davenquinn.com>
2019-05-13 14:09:49 -0400

    Successfully import IGSN data

--- import-pipelines/boise-state/boise_state_importer/igsn_importer.py
+++ import-pipelines/boise-state/boise_state_importer/igsn_importer.py
@@ -1,12 +1,15 @@
 from sparrow.import_helpers import BaseImporter
 from json import dumps
+from click import echo
 import html
 
 class IGSNImporter(BaseImporter):
     def import_data(self, all=True):
         q = self.db.session.query(self.db.model.igsn_data)
-        for igsn_data in q:
+        self.num = 0
+        for i, igsn_data in enumerate(q):
             self.import_igsn(igsn_data)
+        echo(f"Imported {self.num} of {i+1} measurements")
 
     def import_igsn(self, model):
         data = model.data
@@ -14,6 +17,7 @@ class IGSNImporter(BaseImporter):
         s = data['sample']
         # Data with 'status' set are currently embargoed
         if 'status' in s: return
+        self.num += 1
         v = dumps(s, indent=4, sort_keys=True)
         print(v)
 
@@ -31,11 +35,14 @@ class IGSNImporter(BaseImporter):
         if not name:
             name = igsn
 
-        sample = self.sample(
-            igsn=igsn,
-            defaults=dict(
-                id=name
-            ))
+        sample = self.sample(igsn=igsn)
+        if not sample.id:
+            sample.id = name
+        sample.location = location
+        sample.location_name = s.get(
+        'primary_location_name', s.get(
+            'location_description', None
+        ))
 
         self.db.session.add(sample)
         self.db.session.commit()
