bc04bce6aff8fdbfdba466ba62c2e66aa07a5c9a
Daven Quinn <dev@davenquinn.com>
2019-05-19 23:37:49 -0500

    Fixed test metadata

--- import-pipelines/WiscAr/saved-scripts/import-test-metadata.sql
+++ import-pipelines/WiscAr/saved-scripts/import-test-metadata.sql
@@ -4,13 +4,17 @@
 -- 2. Updated rock types in `vocabulary.material`
 
 -- 3.
+INSERT INTO vocabulary.material (id)
+SELECT DISTINCT rock_type FROM method_data.schaen_table_2
+ON CONFLICT DO NOTHING;
+
 UPDATE sample SET
   location = ST_SetSRID(ST_MakePoint(lon,lat),4326),
   location_precision = 0,
   material = rock_type,
   location_name = island || ' Island'
 FROM method_data.schaen_table_2
-WHERE sample_id = id;
+WHERE sample_id = name;
 
 -- 4. Named project 'delarof-1', Delarof Islands Magmatic Evolution
 
@@ -19,8 +23,12 @@ WHERE sample_id = id;
 
 -- 6.
 UPDATE session SET
-  project_id = 'delarof-1'
-WHERE sample_id IN (SELECT sample_id FROM method_data.schaen_table_2);
+  project_id = 1
+WHERE sample_id IN (
+  SELECT id
+  FROM sample s
+  JOIN method_data.schaen_table_2
+  ON sample_id = s.name);
 
 /*
 Importing Cretaceous data
@@ -31,10 +39,10 @@ UPDATE sample SET
   location_precision = 50000,
   material = 'bentonite',
   location_name = 'Carbon County, WY'
-WHERE id = 'D2315';
+WHERE name = 'D2315';
 
 /* Projects and samples are linked through the analytical sessions,
    so update those */
-UPDATE session SET
-  project_id = 'cenomanian-turonian-1'
-WHERE sample_id = 'D2315';
+UPDATE session
+SET project_id = 2
+WHERE sample_id = (SELECT id FROM sample WHERE name = 'D2315');
