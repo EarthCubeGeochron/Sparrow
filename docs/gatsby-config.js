2b27beb075a9553d42b6ddb0e02e9fb769d629a1
Daven Quinn <dev@davenquinn.com>
2019-05-23 16:53:01 -0500

    Substantailly reorganized documentation

--- docs/gatsby-config.js
+++ docs/gatsby-config.js
@@ -19,7 +19,7 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        path: `${__dirname}/text`,
+        path: `${__dirname}/src/text`,
         name: "markdown-pages",
       },
     },
