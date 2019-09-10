cf71337597b56b810f24ce4221b56a91cb900cff
Daven Quinn <dev@davenquinn.com>
2019-09-10 01:15:14 -0400

    Really basic runtime tests

--- api-tests/test_api.py
+++ api-tests/test_api.py
@@ -5,8 +5,20 @@ def get(url, *args, **kwargs):
     uri = "http://backend:5000/api/v1"+url
     return requests.get(uri, *args, **kwargs)
 
+routes = [
+    '/session',
+    '/analysis',
+    '/datum'
+]
+
 @pytest.mark.parametrize("path", ["","/"])
 def test_api_base_exists(path):
     res = get(path)
     assert res.status_code == 200
-    assert 'routes' in res.json()
+    body = res.json()
+    assert 'routes' in body
+
+
+def test_datum_exists():
+    res = get("/datum")
+    assert res.status_code == 200
