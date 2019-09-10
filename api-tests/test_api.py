f5232e64c0590f1f5ab92afd1f285d6c75f61d0e
Daven Quinn <dev@davenquinn.com>
2019-09-10 01:01:03 -0400

    Successfully test API frontend

--- api-tests/test_api.py
+++ api-tests/test_api.py
@@ -1,2 +1,12 @@
-def test_true():
-    assert True
+import requests
+import pytest
+
+def get(url, *args, **kwargs):
+    uri = "http://backend:5000/api/v1"+url
+    return requests.get(uri, *args, **kwargs)
+
+@pytest.mark.parametrize("path", ["","/"])
+def test_api_base_exists(path):
+    res = get(path)
+    assert res.status_code == 200
+    assert 'routes' in res.json()
