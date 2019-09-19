929f8b17db3b6114f20dc6138ddb0a3abaf945d6
Daven Quinn <dev@davenquinn.com>
2019-09-19 13:10:56 -0500

    Updated detail panels etc.

--- backend/sparrow/cli.py
+++ backend/sparrow/cli.py
@@ -59,7 +59,7 @@ def create_views(db):
     """
     # Don't need to build the app just for this
     with working_directory(__file__):
-        db.exec_sql("sql/03-create-views.sql")
+        db.exec_sql("database/fixtures/05-views.sql")
 
 @cli.command(name='serve')
 @with_config
