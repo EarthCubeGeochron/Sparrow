ec0a38174edf3d6692ccfac45cde5fbe2dfae26f
Daven Quinn <dev@davenquinn.com>
2019-11-07 15:20:54 -0600

    Added super-basic API

--- backend/sparrow/cosmo_api/__init__.py
+++ backend/sparrow/cosmo_api/__init__.py
@@ -1,6 +1,13 @@
 from sparrow.plugins import SparrowPlugin
+from flask import Blueprint
+
+cosmo = Blueprint('cosmo_api', __name__)
+
+@cosmo.route('/')
+def index():
+    return "Hello from COSMO API"
 
 class CosmoPlugin(SparrowPlugin):
     name = "cosmo-api"
     def on_api_initialized(self, api):
-        pass
+        self.app.register_blueprint(cosmo, url_prefix='/cosmo-api')
