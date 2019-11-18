952012b4161e0ef9d4053e26df426892b9ba5aaa
Daven Quinn <dev@davenquinn.com>
2019-11-18 16:55:11 -0600

    Better error boundary

--- frontend/src/util/error-boundary.js
+++ frontend/src/util/error-boundary.js
@@ -1,14 +1,15 @@
 import React from 'react'
+import {Callout} from '@blueprintjs/core'
 
 class ErrorBoundary extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { hasError: false };
+    this.state = { error: null};
   }
 
   static getDerivedStateFromError(error) {
     // Update state so the next render will show the fallback UI.
-    return { hasError: true };
+    return { error };
   }
 
   componentDidCatch(error, info) {
@@ -17,9 +18,15 @@ class ErrorBoundary extends React.Component {
   }
 
   render() {
-    if (this.state.hasError) {
+
+    if (this.state.error != null) {
+      let {description} = this.props;
+      const {error} = this.state;
+      if (description == null) description = error.toString();
       // You can render any custom fallback UI
-      return <h1>Something went wrong.</h1>;
+      return <Callout title={"A rendering error occurred"} icon='error' intent='danger'>
+        <p>{description}</p>
+      </Callout>;
     }
 
     return this.props.children;
