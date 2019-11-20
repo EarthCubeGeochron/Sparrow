7e559b00ea0f872808e9bd3d7e258d59bf855b4f
Daven Quinn <dev@davenquinn.com>
2019-11-20 02:34:44 -0600

    Successfully load pgmemento schema

--- backend/core_plugins/versioning/__init__.py
+++ backend/core_plugins/versioning/__init__.py
@@ -1,6 +1,18 @@
 from sparrow.plugins import SparrowCorePlugin
+from sparrow.util import relative_path
 
 class VersioningPlugin(SparrowCorePlugin):
     name = "versioning"
     def on_core_tables_initialized(self, db):
-        print("SSSSS")
+        files = [
+            "src/SCHEMA.sql",
+            "src/SETUP.sql",
+            "src/LOG_UTIL.sql",
+            "src/DDL_LOG.sql",
+            "src/RESTORE.sql",
+            "src/REVERT.sql",
+            "src/SCHEMA_MANAGEMENT.sql"
+        ]
+        for fn in files:
+            fp = relative_path(__file__, 'pg-memento', fn)
+            db.exec_sql(fp)
