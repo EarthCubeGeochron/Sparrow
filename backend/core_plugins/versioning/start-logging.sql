96e48bfc18edd86713e9fb73e0155a58dbb49654
Daven Quinn <dev@davenquinn.com>
2020-02-05 17:38:42 -0600

    Added comment

--- backend/core_plugins/versioning/start-logging.sql
+++ backend/core_plugins/versioning/start-logging.sql
@@ -3,4 +3,8 @@ SELECT pgmemento.create_schema_event_trigger(TRUE);
 
 -- Start auditing for tables in `public` schema
 SELECT pgmemento.create_schema_audit(
-  'public', TRUE, ARRAY['spatial_ref_sys']);
+  'public',
+  TRUE,
+  -- Tables to ignore
+  ARRAY['spatial_ref_sys']
+);
