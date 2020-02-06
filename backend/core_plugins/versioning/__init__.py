9f4d4e7833bbd33e3570931de49f2cdfc17f3db3
Daven Quinn <dev@davenquinn.com>
2020-02-05 18:50:29 -0600

    Added basic functions to recreate auditing

--- backend/core_plugins/versioning/__init__.py
+++ backend/core_plugins/versioning/__init__.py
@@ -1,5 +1,29 @@
 from sparrow.plugins import SparrowCorePlugin
 from sparrow.util import relative_path
+from sparrow import App
+import click
+from sqlalchemy import inspect, sql
+from sparrow.util import run_sql
+
+exclude_tables = ['spatial_ref_sys']
+
+@click.command(name="remove-audit-trail")
+def drop_audit_trail():
+    """
+    Remove PGMemento audit trail
+    """
+    db = App(__name__).database
+    db.exec_sql(relative_path(__file__, 'drop-audit.sql'))
+    insp = inspect(db.engine)
+    for table in insp.get_table_names(schema='public'):
+        if table in exclude_tables:
+            continue
+        cols = insp.get_columns(table, schema='public')
+        col_names = [c['name'] for c in cols]
+        if 'audit_id' not in col_names:
+            continue
+        q = f"ALTER TABLE {table} DROP COLUMN audit_id CASCADE"
+        run_sql(db.session, q)
 
 class VersioningPlugin(SparrowCorePlugin):
     name = "versioning"
@@ -17,3 +41,6 @@ class VersioningPlugin(SparrowCorePlugin):
             fp = relative_path(__file__, 'pg-memento', fn)
             db.exec_sql(fp)
         db.exec_sql(relative_path(__file__,'start-logging.sql'))
+
+    def on_setup_cli(self, cli):
+        cli.add_command(drop_audit_trail)
