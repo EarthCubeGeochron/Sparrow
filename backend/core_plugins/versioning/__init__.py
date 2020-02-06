b175682ddbc0d37966ed42272fe2266d1277e461
Daven Quinn <dev@davenquinn.com>
2020-02-06 01:59:06 -0600

    More concise table init function for sqlalchemy

--- backend/core_plugins/versioning/__init__.py
+++ backend/core_plugins/versioning/__init__.py
@@ -8,11 +8,21 @@ from sparrow.util import run_sql
 exclude_tables = ['spatial_ref_sys']
 audit_schemas = ['public', 'vocabulary']
 
+# Tables in the pg_memento schema
+memento_tables = [
+    'audit_column_log',
+    'audit_table_log',
+    'table_event_log',
+    'transaction_log',
+    'row_log'
+]
+
 def has_audit_id(db, schema, table):
     insp = inspect(db.engine)
     cols = insp.get_columns(table, schema=schema)
     col_names = [c['name'] for c in cols]
     return 'audit_id' in col_names
+
 
 def audit_tables(db):
     insp = inspect(db.engine)
@@ -24,19 +34,24 @@ def audit_tables(db):
                 continue
             yield schema, table
 
+
+def has_audit_schema(db):
+    insp = inspect(db.engine)
+    realized_tables = insp.get_table_names(schema='pgmemento')
+    # Some of the pg_memento tables are not created...
+    return all(t in realized_tables for t in memento_tables)
+
+
 def drop_audit_columns(db):
     for schema, table in audit_tables(db):
         q = f"ALTER TABLE {schema}.{table} DROP COLUMN audit_id CASCADE"
         run_sql(db.session, q)
 
-# There is a bug where audit-trail columns
-# lose default values, causing INSERTS to be rejected
-# across-the-board...
-# This appears to happen on schema setup.
-# May need to reapply defaults to audit columns:
-# ALTER COLUMN audit_id SET DEFAULT
-#   nextval(''pgmemento.audit_id_seq''::regclass)
+
 def add_audit_id_sequence(db):
+    """
+    Re-add audit sequences (this seems necessary sometimes)
+    """
     for schema, table in audit_tables(db):
         q = (f"ALTER TABLE {schema}.{table} "
              "ALTER COLUMN audit_id SET DEFAULT "
@@ -54,21 +69,27 @@ def drop_audit_trail():
 
 class VersioningPlugin(SparrowCorePlugin):
     name = "versioning"
+
+    def proc(self, id):
+        fn = id+'.sql'
+        return
+
     def on_core_tables_initialized(self, db):
-        files = [
-            "src/SCHEMA.sql",
-            "src/SETUP.sql",
-            "src/LOG_UTIL.sql",
-            "src/DDL_LOG.sql",
-            "src/RESTORE.sql",
-            "src/REVERT.sql",
-            "src/SCHEMA_MANAGEMENT.sql"
-        ]
-        for fn in files:
-            fp = relative_path(__file__, 'pg-memento', fn)
+
+        # Basic setup procedures
+        procedures = ["SETUP", "LOG_UTIL", "DDL_LOG",
+                      "RESTORE", "REVERT", "SCHEMA_MANAGEMENT"]
+
+        if not has_audit_schema(db):
+            # Create the schema to hold audited tables
+            # NOTE: this drops all transaction history, so we don't run
+            # it if pgMemento tables already exist.
+            procedures.insert("SCHEMA", 0)
+
+        for id in procedures:
+            fp = relative_path(__file__, 'pg-memento', 'src', id+'.sql')
             db.exec_sql(fp)
-        db.exec_sql(relative_path(__file__,'start-logging.sql'))
-        add_audit_id_sequence(db)
+        db.exec_sql(relative_path(__file__, 'start-logging.sql'))
 
     def on_setup_cli(self, cli):
         cli.add_command(drop_audit_trail)
--- backend/core_plugins/versioning/start-logging.sql
+++ backend/core_plugins/versioning/start-logging.sql
@@ -4,14 +4,16 @@ SELECT pgmemento.create_schema_event_trigger(TRUE);
 -- Start auditing for tables in `public` schema
 SELECT pgmemento.create_schema_audit(
   'public',
-  TRUE,
+  -- Log existing data as imported
+  FALSE,
   -- Tables to ignore
   ARRAY['spatial_ref_sys']
 );
 
 SELECT pgmemento.create_schema_audit(
   'vocabulary',
-  TRUE,
+  -- Log existing data as imported
+  FALSE,
   -- Tables to ignore
   null
 );
