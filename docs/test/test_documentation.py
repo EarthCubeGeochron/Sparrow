b247c8379f9c1e9a5883a9416e04fc6e4331c979
Daven Quinn <dev@davenquinn.com>
2020-06-20 21:14:00 -0500

    Documentation testing is improved

--- docs/test/test_documentation.py
+++ docs/test/test_documentation.py
@@ -1,18 +1,20 @@
 import requests
 import pytest
 
+
 def get(url, *args, **kwargs):
-    uri = "http://frontend:8000"+url
+    uri = "http://frontend:8000" + url
     return requests.get(uri, *args, **kwargs)
 
 
 routes = [
-    '/',
-    '/docs',
-    '/motivation-and-design',
-    '/installation',
-    '/schema'
+    "/",
+    "/docs",
+    "/docs/motivation-and-design",
+    "/docs/getting-started",
+    "/docs/schema",
 ]
+
 
 @pytest.mark.parametrize("route", routes)
 def test_route_exists(route):
