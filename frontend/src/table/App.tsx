d198b91d8487f4b063f1b4b6a4531a416208cd7d
Daven Quinn <dev@davenquinn.com>
2020-07-16 15:01:41 -0500

    Removed ui-components submodule (probably temporary) to fix hooks loading

--- frontend/src/table/App.tsx
+++ frontend/src/table/App.tsx
@@ -5,46 +5,8 @@ import {
   EditableNumCell,
 } from "./Components/EditableCell/EditableCell";
 import {
-  APIContext,
-  APIActions,
-  QueryParams,
-  APIHookOpts,
+  useAPIResult
 } from "@macrostrat/ui-components";
-
-const useAPIResult = function <T>(
-  route: string | null,
-  params: QueryParams = {},
-  opts: APIHookOpts | (<T, U = any>(arg: U) => T) = {}
-): T {
-  /* React hook for API results, pulled out of @macrostrat/ui-components.
-  The fact that this works inline but the original hook doesn't suggests that we
-  have overlapping React versions between UI-components and the main codebase.
-  Frustrating. */
-  const deps = [route, ...Object.values(params ?? {})];
-
-  const [result, setResult] = useState<T | null>(null);
-
-  console.log("Your face");
-
-  if (typeof opts === "function") {
-    opts = { unwrapResponse: opts };
-  }
-
-  const { debounce: _debounce, ...rest } = opts ?? {};
-  let { get } = APIActions(useContext(APIContext));
-
-  useEffect(() => {
-    const getAPIData = async function () {
-      if (route == null) {
-        return setResult(null);
-      }
-      const res = await get(route, params, rest);
-      return setResult(res);
-    };
-    getAPIData();
-  }, deps);
-  return result;
-};
 
 const Table = function (props) {
   const [data, setData] = useState([]);
