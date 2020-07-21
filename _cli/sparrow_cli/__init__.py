529e548b4e8efdf1ca4d1d0a2ef4ee9920ef98e3
Daven Quinn <dev@davenquinn.com>
2020-07-21 00:04:16 -0500

    Updated docker-compose files

--- _cli/sparrow_cli/__init__.py
+++ _cli/sparrow_cli/__init__.py
@@ -20,7 +20,7 @@ from .test import sparrow_test
 
 def find_config_file(dir: Path) -> Optional[Path]:
     for folder in (dir, *dir.parents):
-        pth = folder/"sparrow-config.sh"
+        pth = folder / "sparrow-config.sh"
         if pth.is_file():
             return pth
 
@@ -33,6 +33,7 @@ def get_config() -> Optional[Path]:
         return None
     return Path(__config)
 
+
 console = Console(highlight=True)
 
 
@@ -40,19 +41,19 @@ def find_subcommand(directories, name):
     if name is None:
         return None
     for dir in directories:
-        fn = dir/("sparrow-"+name)
+        fn = dir / ("sparrow-" + name)
         if fn.is_file():
             return str(fn)
 
-@click.command("sparrow", context_settings=dict(
-    ignore_unknown_options=True,
-    help_option_names=[],
-))
-@click.argument('args', nargs=-1, type=click.UNPROCESSED)
+
+@click.command(
+    "sparrow", context_settings=dict(ignore_unknown_options=True, help_option_names=[],)
+)
+@click.argument("args", nargs=-1, type=click.UNPROCESSED)
 @click.pass_context
 def cli(ctx, args):
-    environ['SPARROW_WORKDIR'] = getcwd()
-    here = Path(environ['SPARROW_WORKDIR'])
+    environ["SPARROW_WORKDIR"] = getcwd()
+    here = Path(environ["SPARROW_WORKDIR"])
 
     sparrow_config = get_config()
 
@@ -62,40 +63,43 @@ def cli(ctx, args):
 
     if sparrow_config is None:
         echo("No configuration file found. Running using default values.", err=True)
-        environ['_SPARROW_CONFIG_UNSET'] = '1'
+        environ["_SPARROW_CONFIG_UNSET"] = "1"
     else:
-        environ['SPARROW_CONFIG'] = str(sparrow_config)
-        environ['SPARROW_CONFIG_DIR'] = str(sparrow_config.parent)
+        environ["SPARROW_CONFIG"] = str(sparrow_config)
+        environ["SPARROW_CONFIG_DIR"] = str(sparrow_config.parent)
 
     _config_sourced = environ.get("_SPARROW_CONFIG_SOURCED", "0") == "1"
 
     if sparrow_config is not None and not _config_sourced:
-        chdir(environ['SPARROW_CONFIG_DIR'])
+        chdir(environ["SPARROW_CONFIG_DIR"])
         # This requires bash to be available on the platform, which
         # might be a problem for Windows/WSL.
-        load_envbash(environ['SPARROW_CONFIG'])
+        load_envbash(environ["SPARROW_CONFIG"])
         # Change back to original working directory
-        chdir(environ['SPARROW_WORKDIR'])
+        chdir(environ["SPARROW_WORKDIR"])
         environ["_SPARROW_CONFIG_SOURCED"] = "1"
 
     # Check if this script is part of a source
     # installation. If so, set SPARROW_PATH accordingly
-    is_frozen = getattr( sys, 'frozen', False )
+    is_frozen = getattr(sys, "frozen", False)
     if "SPARROW_PATH" not in environ:
         this_exe = Path(__file__).resolve()
         if not is_frozen:
             pth = this_exe.parent.parent.parent
-            environ['SPARROW_PATH'] = str(pth)
+            environ["SPARROW_PATH"] = str(pth)
 
     bin_directories = []
 
-    if 'SPARROW_PATH' in environ:
-        bin = Path(environ['SPARROW_PATH'])/'bin'
+    if "SPARROW_PATH" in environ:
+        bin = Path(environ["SPARROW_PATH"]) / "bin"
         bin_directories.append(bin)
     else:
-        secho("Sparrow could not automatically find a the source directory. "
-              "Running without a local installation is not yet supported. "
-              "Please set SPARROW_PATH to the location of the cloned Sparrow repository.", fg='red')
+        secho(
+            "Sparrow could not automatically find a the source directory. "
+            "Running without a local installation is not yet supported. "
+            "Please set SPARROW_PATH to the location of the cloned Sparrow repository.",
+            fg="red",
+        )
         sys.exit(1)
 
     # ENVIRONMENT VARIABLE DEFAULTS
@@ -105,7 +109,6 @@ def cli(ctx, args):
     environ.setdefault("SPARROW_BASE_URL", "/")
     environ.setdefault("SPARROW_LAB_NAME", "My Lab")
 
-
     # Make sure all internal commands can be referenced by name from
     # within Sparrow (even if `sparrow` command itself isn't on the PATH)
     __cmd = environ.get("SPARROW_COMMANDS")
@@ -114,10 +117,12 @@ def cli(ctx, args):
 
     # Add location of Sparrow commands to path
     __added_path_dirs = [str(i) for i in bin_directories]
-    environ['PATH'] = ":".join([*__added_path_dirs, environ["PATH"]])
+    environ["PATH"] = ":".join([*__added_path_dirs, environ["PATH"]])
 
     if environ.get("SPARROW_SECRET_KEY") is None:
-        print("[red]You [underline]must[/underline] set [bold]SPARROW_SECRET_KEY[/bold]. Exiting...")
+        print(
+            "[red]You [underline]must[/underline] set [bold]SPARROW_SECRET_KEY[/bold]. Exiting..."
+        )
         sys.exit(1)
 
     rest = []
@@ -130,10 +135,10 @@ def cli(ctx, args):
         echo_help(*bin_directories)
         sys.exit(0)
 
-    if subcommand == 'compose':
+    if subcommand == "compose":
         return compose(*rest)
 
-    if subcommand == 'test':
+    if subcommand == "test":
         return ctx.invoke(sparrow_test, *rest)
 
     _command = find_subcommand(bin_directories, subcommand)
--- _cli/sparrow_cli/util.py
+++ _cli/sparrow_cli/util.py
@@ -1,6 +1,7 @@
 from os import environ, chdir, path
 from subprocess import run, PIPE, STDOUT
 from shlex import split
+from click import secho
 
 
 def cmd(*v, **kwargs):
@@ -9,12 +10,37 @@ def cmd(*v, **kwargs):
 
 
 def compose(*args, **kwargs):
-    base = environ['SPARROW_PATH']
+    base = environ["SPARROW_PATH"]
     main = path.join(base, "docker-compose.yaml")
+
+    compose_files = [main]
+
+    env = environ.get("SPARROW_ENV", "production")
+    if env == "production":
+        compose_files.append(path.join(base, "docker-compose.production.yaml"))
+
+        if environ.get("CERTBOT_EMAIL") is not None:
+            # We want to use certbot NGINX configuration
+            compose_files.append(path.join(base, "docker-compose.certbot.yaml"))
+
+    # Overrides should now be formatted as a COMPOSE_FILE colon-separated list
     overrides = environ.get("SPARROW_COMPOSE_OVERRIDES", "")
-    # Overrides should be deprecated in favor of COMPOSE_FILE colon-separated list
+    if overrides.startswith("-f "):
+        secho(
+            "You are using a deprecated signature for the SPARROW_COMPOSE_OVERRIDES "
+            "environment variable. This option should now be formatted as a "
+            "colon-separated path similar to the COMPOSE_FILE docker-compose "
+            "configuration parameter (https://docs.docker.com/compose/reference/envvars/#compose_file)",
+            fg="yellow",
+            err=True,
+        )
+    elif overrides != "":
+        compose_files += overrides.split(":")
+
+    environ["COMPOSE_FILE"] = ":".join(compose_files)
+
     chdir(base)
-    return cmd("docker-compose", "-f", main, overrides, *args, **kwargs)
+    return cmd("docker-compose", overrides, *args, **kwargs)
 
 
 def container_is_running(name):
