ac1fea14a8586ca6abc1612d0531a570047757dc
Daven Quinn <dev@davenquinn.com>
2020-08-07 21:25:28 -0500

    Moved homepage component back to ts

---
+++ frontend/src/homepage.ts
@@ -0,0 +1,18 @@
+import { hyperStyled } from "@macrostrat/hyper";
+import { Frame } from "./frame";
+import { InsetText } from "app/layout";
+
+import styles from "./styles.module.css";
+
+const h = hyperStyled(styles);
+
+const HomePage = () => {
+  return h("div.homepage", [
+    h(InsetText, [
+      h(Frame, { id: "landingText" }),
+      h(Frame, { id: "landingGraphic" }),
+    ]),
+  ]);
+};
+
+export { HomePage };
