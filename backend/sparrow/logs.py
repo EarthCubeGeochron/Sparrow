1607bbc257d1c4c857ba43fef2aa07b5da1a6ecd
Daven Quinn <dev@davenquinn.com>
2020-08-15 23:23:33 -0500

    Fixed auto-reloading and application logging

--- backend/sparrow/logs.py
+++ backend/sparrow/logs.py
@@ -1,16 +1,32 @@
 import logging
+from colorlog import StreamHandler, ColoredFormatter
+from uvicorn.logging import DefaultFormatter
+
+# disabled 'levelname'
+formatter = ColoredFormatter(
+    "%(log_color)s%(name)s:%(reset)s    %(message)s",
+    datefmt=None,
+    reset=True,
+    log_colors={
+        "DEBUG": "cyan",
+        "INFO": "green",
+        "WARNING": "yellow",
+        "ERROR": "red",
+        "CRITICAL": "red,bg_white",
+    },
+    secondary_log_colors={},
+    style="%",
+)
 
 
 def console_handler():
-    console = logging.StreamHandler()
+    console = StreamHandler()
     # create console handler and set level to debug
     console.setLevel(logging.DEBUG)
     # create formatter
-    formatter = logging.Formatter(
-        "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
-    )
     # add formatter to ch
     console.setFormatter(formatter)
+    return console
 
 
 def get_logger(name, level=logging.DEBUG, handler=None):
@@ -19,4 +35,7 @@ def get_logger(name, level=logging.DEBUG, handler=None):
     # add ch to logger
     if handler:
         log.addHandler(handler)
+    else:
+        # This is likely to be slow
+        log.addHandler(console_handler())
     return log
