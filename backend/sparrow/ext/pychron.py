427fc92dbd970da01a0afabca1d6a8c4ce29d0b9
Daven Quinn <dev@davenquinn.com>
2020-09-01 12:40:02 -0500

    Serialize basic PyChron data file

--- backend/sparrow/ext/pychron.py
+++ backend/sparrow/ext/pychron.py
@@ -8,6 +8,10 @@ class PyChronJSONImporter:
     def __init__(self):
         pass
 
+    def datum_type_for(self, key):
+        # Right now we phone this in with "dimensionless" for everything
+        return {"unit": "dimensionless", "parameter": key}
+
     def transform_datum(self, analysis, key):
         v = analysis.pop(key)
         e = analysis.pop(key + "_err", None)
@@ -16,14 +20,15 @@ class PyChronJSONImporter:
         return {
             "value": v,
             "error": e,
-            "type": key,
+            "type": self.datum_type_for(key),
         }
 
     def transform_analysis(self, analysis):
         data = []
         for k in ["age", "kca", "kcl", "radiogenic_yield"]:
             data.append(self.transform_datum(analysis, k))
-        return {"name": analysis.pop("record_id"), "data": data}
+        # TODO: allow UUIDs to be created in Analysis model aswell.
+        return {"analysis_name": analysis.pop("record_id"), "data": data}
 
     def transform_sample(self, sample):
         lat = sample.get("latitude")
@@ -42,20 +47,24 @@ class PyChronJSONImporter:
             d = self.transform_datum(ages, age + "_age")
             if d is not None:
                 datum.append(d)
-        return {"datum": datum, "type": "Ages"}
+        return {"datum": datum, "analysis_name": "Ages"}
 
     def import_file(self, data):
+        """Build basic nested JSON representation of a PyChron IA file."""
         analyses = [self.transform_analysis(a) for a in data["analyses"]]
         analyses.append(self.transform_ages(data["preferred"]["ages"]))
 
         res = {"analysis": analyses}
 
         # Copy a few fields directly:
+        # NOTE: I am unsure if we should hijack Sparrow's internal UUID field for PyChron
+        # ids, or create a new field for this in the PyChron Importer Plugin. I lean towards
+        # the former.
         for k in ["name", "uuid"]:
-            res[k] = data[k]
+            res[k] = str(data[k])
 
         # There is no _DATE_ in the IA file!
-        res["date"] = datetime.min
+        res["date"] = datetime.min.isoformat()
 
         res["sample"] = self.transform_sample(data["sample_metadata"])
 
