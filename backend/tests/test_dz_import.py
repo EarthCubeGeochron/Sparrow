9f02052d54a5321687434f7090a86fee2134c124
Daven Quinn <dev@davenquinn.com>
2020-09-02 04:35:43 -0500

    Non-functional additional way to get dz import

--- backend/tests/test_dz_import.py
+++ backend/tests/test_dz_import.py
@@ -1,21 +1,29 @@
 from sparrow.util import relative_path
 from sparrow.ext.detrital_zircon import DetritalZirconTableImporter
 from pandas import read_csv
-from pytest import mark
-from rich import print
+from pytest import mark, fixture
+
+
+@fixture(scope="module")
+def dz_data():
+    """Importing ~300 DZ measurements is slow"""
+    importer = DetritalZirconTableImporter()
+    fn = relative_path(__file__, "fixtures", "detrital-zircon-F-90.csv")
+    df = read_csv(fn)
+    yield importer(df)
 
 
 @mark.slow
-def test_dz_import(db):
-    fn = relative_path(__file__, "fixtures", "detrital-zircon-F-90.csv")
-    df = read_csv(fn)
+def test_dz_import(db, dz_data):
+    db.load_data("session", dz_data)
 
-    importer = DetritalZirconTableImporter()
-    res = importer(df)
 
-    analysis_list = res.pop("analysis")
+@mark.xfail(reason="This doesn't work yet.")
+def test_dz_import_iterative(db, dz_data):
+    """A potentially quicker method to add analyses after creation"""
+    analysis_list = dz_data.pop("analysis")
 
-    session = db.load_data("session", res)
+    session = db.load_data("session", dz_data)
     assert session.id is not None
 
     for a in analysis_list:
