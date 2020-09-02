be10c6a97d0ae40f3e559bb5cd1757508560d1f4
Daven Quinn <dev@davenquinn.com>
2020-09-02 04:26:17 -0500

    Try to go a more iterative direction

--- backend/tests/test_dz_import.py
+++ backend/tests/test_dz_import.py
@@ -2,6 +2,7 @@ from sparrow.util import relative_path
 from sparrow.ext.detrital_zircon import DetritalZirconTableImporter
 from pandas import read_csv
 from pytest import mark
+from rich import print
 
 
 @mark.slow
@@ -11,4 +12,11 @@ def test_dz_import(db):
 
     importer = DetritalZirconTableImporter()
     res = importer(df)
-    db.load_data("session", res)
+
+    analysis_list = res.pop("analysis")
+
+    session = db.load_data("session", res)
+    assert session.id is not None
+
+    for a in analysis_list:
+        db.load_data("analysis", dict(session=session.id, **a))
