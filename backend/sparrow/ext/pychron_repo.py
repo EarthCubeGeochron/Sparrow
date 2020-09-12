1b930265614e960014e82bdf6ff20c0a282fa529
Jake Ross <jirhiker@gmail.com>
2020-09-11 19:53:31 -0600

    started pychron repo scanner

---
+++ backend/sparrow/ext/pychron_repo.py
@@ -0,0 +1,61 @@
+# ===============================================================================
+# Copyright 2020 ross
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ===============================================================================
+import os
+import subprocess
+
+try:
+    from backend.sparrow.ext.pychron import PyChronJSONImporter
+except ImportError:
+    class PyChronJSONImporter:
+        def import_file(self, *args, **kw):
+            pass
+
+
+class PychronRepo:
+    def __init__(self, name, remote):
+        local_repos_root = os.path.join(os.path.expanduser('~'), 'local_repo')
+        if not os.path.isdir(local_repos_root):
+            os.mkdir(local_repos_root)
+
+        self._root = os.path.join(local_repos_root, name)
+        self._url = '{}/{}'.format(remote, name)
+
+    def scan(self):
+        # check if repo exists
+        # clone otherwise
+
+        root = self._root
+        if not os.path.isdir(root):
+            subprocess.run(['git', 'clone', self._url, root])
+
+        importer = PyChronJSONImporter()
+        # scan repo looking for ia files
+        for d, ds, fs in os.walk(root):
+            for fi in fs:
+                if fi.endswith('.ia.json'):
+                    p = os.path.join(d, fi)
+                    print('importing file. {}'.format(p))
+                    importer.import_file(p)
+
+
+if __name__ == '__main__':
+
+    name = 'NOB-Unknowns'
+    remote = 'https://github.com/WiscArData'
+    pr = PychronRepo(name, remote)
+    pr.scan()
+
+# ============= EOF =============================================
