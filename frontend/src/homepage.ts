213d70f6d0652475ea8400abf9d8e0315f105da5
Casey Idzikowski <casey_idz@macbook-pro.home>
2020-09-29 15:33:48 -0500

    Zoomable Globe easy override

--- frontend/src/homepage.ts
+++ frontend/src/homepage.ts
@@ -4,9 +4,10 @@ import { InsetText } from "app/components/layout";
 // @ts-ignore
 import styles from "./styles.module.css";
 import loadable from "@loadable/component";
+import { SampleMap } from "../plugins/globe";
 import { useContext } from "react";
 
-const MapHome = loadable(async function () {
+const MapHome = loadable(async function() {
   const module = await import("./map");
   return module.MapHome;
 });
@@ -16,7 +17,11 @@ const h = hyperStyled(styles);
 const HomePage = () => {
   return h("div.homepage", [
     h(InsetText, [
-      h(Frame, { id: "landingText" }),
+      h(
+        Frame,
+        { id: "landingText" },
+        "This is where your lab's description text can go"
+      ),
       h(Frame, { id: "landingGraphic" }, h(MapHome)),
     ]),
   ]);
