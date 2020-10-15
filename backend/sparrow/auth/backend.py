b420643b825ccba9efb794b39eb5725cc2002e82
Daven Quinn <dev@davenquinn.com>
2020-10-15 16:39:24 -0500

    Collected some resources and prior art

---
+++ backend/sparrow/auth/backend.py
@@ -0,0 +1,56 @@
+from starlette.authentication import (
+    AuthenticationBackend,
+    AuthenticationError,
+    BaseUser,
+    AuthCredentials,
+    UnauthenticatedUser,
+)
+
+
+class JWTAuthenticationBackend(AuthenticationBackend):
+    def __init__(
+        self,
+        secret_key: str,
+        algorithm: str = "HS256",
+        prefix: str = "JWT",
+        username_field: str = "username",
+    ):
+        self.secret_key = secret_key
+        self.algorithm = algorithm
+        self.prefix = prefix
+        self.username_field = username_field
+
+    @classmethod
+    def get_token_from_header(cls, authorization: str, prefix: str):
+        """
+        Parses the Authorization header and returns only the token
+        :param authorization:
+        :return:
+        """
+        try:
+            scheme, token = authorization.split()
+        except ValueError:
+            raise AuthenticationError(
+                "Could not separate Authorization scheme and token"
+            )
+        if scheme.lower() != prefix.lower():
+            raise AuthenticationError(f"Authorization scheme {scheme} is not supported")
+        return token
+
+    async def authenticate(self, request):
+        if "Authorization" not in request.headers:
+            return None
+
+        auth = request.headers["Authorization"]
+        token = self.get_token_from_header(authorization=auth, prefix=self.prefix)
+        try:
+            payload = jwt.decode(token, key=self.secret_key, algorithms=self.algorithm)
+        except jwt.InvalidTokenError as e:
+            raise AuthenticationError(str(e))
+
+        return (
+            AuthCredentials(["authenticated"]),
+            JWTUser(
+                username=payload[self.username_field], token=token, payload=payload
+            ),
+        )
