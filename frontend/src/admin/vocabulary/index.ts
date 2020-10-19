0833b8687aef2a90f17a8c4c7bd623056c27160b
Daven Quinn <dev@davenquinn.com>
2020-10-19 12:26:43 -0500

    Fixed excessive re-renders

--- frontend/src/admin/vocabulary/index.ts
+++ frontend/src/admin/vocabulary/index.ts
@@ -1,8 +1,25 @@
 import { hyperStyled } from "@macrostrat/hyper";
 import { useAPIv2Result } from "~/api-v2";
-import { Spinner, Card } from "@blueprintjs/core";
+import { useState } from "react";
+import {
+  Spinner,
+  Card,
+  Button,
+  ButtonGroup,
+  Menu,
+  MenuItem,
+  Popover,
+} from "@blueprintjs/core";
+import { Switch, Route, useRouteMatch } from "react-router-dom";
+import { MinimalNavbar, NavButton } from "~/components";
+
 import styles from "./module.styl";
 const h = hyperStyled(styles);
+
+enum Term {
+  PARAMETER = "parameter",
+  MATERIAL = "material",
+}
 
 function TermCard(props) {
   const { data } = props;
@@ -16,9 +33,9 @@ function TermCard(props) {
 }
 
 function VocabularyList(props) {
-  const { authority, exclude, local = false } = props;
+  const { authority, exclude, local = false, term = Term.PARAMETER } = props;
 
-  const params = useAPIv2Result("/vocabulary/parameter", {
+  const params = useAPIv2Result(`/vocabulary/${term}`, {
     all: true,
     authority,
   });
@@ -37,9 +54,64 @@ function VocabularyList(props) {
   ]);
 }
 
+function NavMenu() {
+  return h(ButtonGroup, { minimal: true }, [
+    h(NavButton, { to: "/admin/terms/parameter" }, "Parameter"),
+    h(NavButton, { to: "/admin/terms/material" }, "Material"),
+  ]);
+}
+
+function AuthorityMenu({ authority, setAuthority }) {
+  const auth = useAPIv2Result("/vocabulary/authority", { all: true }, {});
+  if (auth == null) return null;
+  const content = h(
+    Menu,
+    {},
+    auth.data.map((d) =>
+      h(MenuItem, {
+        onClick() {
+          setAuthority(d.id);
+        },
+        text: d.id,
+      })
+    )
+  );
+
+  return h(
+    Popover,
+    { content },
+    h(Button, { minimal: true }, h(["Authority: ", h("b", authority)]))
+  );
+}
+
+function VocabularyPanel({ authority }) {
+  const term = useRouteMatch()?.params?.term ?? "parameter";
+
+  return h("div.vocabulary-panel", [
+    h(VocabularyList, { local: true, term }),
+    h(VocabularyList, { authority, term }),
+  ]);
+}
+
 export function VocabularyPage() {
+  const [authority, setAuthority] = useState("Sparrow");
+  const base = "/admin/terms";
+
   return h("div.vocabulary-page", {}, [
-    h(VocabularyList, { local: true }),
-    h(VocabularyList, { authority: "Sparrow" }),
+    h(MinimalNavbar, { className: "navbar" }, [
+      h("h4", "Terms"),
+      h(NavMenu),
+      h("div.navbar-spacer"),
+      h(AuthorityMenu, { authority, setAuthority }),
+    ]),
+    h(Switch, [
+      h(
+        Route,
+        {
+          path: base + "/:term",
+        },
+        h(VocabularyPanel, { authority })
+      ),
+    ]),
   ]);
 }
