b6b5b1ea730ee29e3b7c433dd41072fb274d809d
Casey Idzikowski <casey_idz@MacBook-Pro.local>
2020-10-26 17:32:22 -0400

    api tests

--- backend/sparrow_tests/test_api.py
+++ backend/sparrow_tests/test_api.py
@@ -13,12 +13,19 @@ class TestAPIV2:
     @mark.xfail(
         reason="This fails due to transaction isolation in the testing database."
     )
+    def test_api_models_sample(self, client):
+        """Practice with Python Tests"""
+        res = client.get("/api/v2/models/")
+        assert res.status_code == 200
+        data = res.json()
+        assert data["description"] is not None
+
     def test_get_data(self, client, db):
         """Get some data for us to work with"""
         db.load_data("session", basic_data)
 
         res = client.get("/api/v2/session")
-        assert res.status_code == 200
+        assert res.status_code == 200 ## this breaks
         data = res.json()
         assert data["total_count"] == 1
         assert data["data"][0]["name"] == "Declarative import test"
