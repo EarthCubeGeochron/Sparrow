f8c058ffb064fba1b66dce31a75b498989ae1a2d
Daven Quinn <dev@davenquinn.com>
2020-11-12 03:02:24 -0600

    Fixed settings function

--- backend/sparrow/settings.py
+++ backend/sparrow/settings.py
@@ -2,9 +2,9 @@ from starlette.config import Config
 
 config = Config()
 
-LAB_NAME = config("SPARROW_LAB_NAME", "Test lab")
-DATABASE = config("SPARROW_DATABASE", "postgresql+psycopg2:///sparrow")
-BASE_URL = config("SPARROW_BASE_URL", "/")
+LAB_NAME = config("SPARROW_LAB_NAME", default="Test lab")
+DATABASE = config("SPARROW_DATABASE", default="postgresql+psycopg2:///sparrow")
+BASE_URL = config("SPARROW_BASE_URL", default="/")
 
 SECRET_KEY = config("SPARROW_SECRET_KEY", None)
 if SECRET_KEY is None:
