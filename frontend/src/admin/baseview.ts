548add23661a6aec05e5bbc7a0805a9f660668e3
Casey Idzikowski <idzikoj1234@gmail.com>
2020-12-06 18:28:13 -0600

    fix camelcase

--- frontend/src/admin/baseview.ts
+++ frontend/src/admin/baseview.ts
@@ -10,7 +10,10 @@ export const AnimatedArrow = () => {
     h(Icon, { icon: "arrow-left", iconSize: 70 }),
   ]);
 };
-
+/**
+ * 
+ * @description Default page when nothing selected on left navigation
+ */
 export function NoStateAdmin(props) {
   const { name } = props;
 
