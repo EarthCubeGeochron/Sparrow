99d240ac4fee21761bcfecbcb44ccb832571abd6
Casey Idzikowski <idzikoj1234@gmail.com>
2020-12-06 18:46:03 -0600

    after beginning to edit these I decided to just remake something else while leaving this apiv1 version

--- backend/core_plugins/__init__.py
+++ backend/core_plugins/__init__.py
@@ -1,9 +1,8 @@
 # flake8: noqa
 from .earthchem_vocabulary import EarthChemVocabularyPlugin
 from .location_names import LocationNamesPlugin
-from .project_edit import ProjectEditPlugin
 from .destructive_operations import DestructiveOperationsPlugin
-
+from .project_edit import ProjectEditPlugin
 # These should only be run by labs that enable them
 from .versioning import VersioningPlugin
 from .import_data import ImportDataPlugin
