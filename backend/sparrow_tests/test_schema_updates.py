d278e0d1af14418b4bad7e4c0066d6b8eae770a2
Daven Quinn <dev@davenquinn.com>
2020-12-11 03:56:12 -0500

    Added passing test of sample creation

--- backend/sparrow_tests/test_schema_updates.py
+++ backend/sparrow_tests/test_schema_updates.py
@@ -1,4 +1,6 @@
 from pytest import mark
+from geoalchemy2.shape import from_shape
+from shapely.geometry import shape
 
 test_sample = {
     "name": "4c1321a",
@@ -8,9 +10,24 @@ test_sample = {
 
 
 class TestSchemaUpdates:
+    def test_load_sample_imperative(self, db):
+        """A simple test to make sure we can load a single sample with
+        a linked material"""
+        Sample = db.model.sample
+        Material = db.model.vocabulary_material
+
+        loc = from_shape(shape(test_sample["location"]))
+
+        mat = Material(id="Granitic rock")
+        obj = Sample(name="3a24aba", location=loc)
+        obj._material = mat
+        db.session.add(obj)
+        db.session.commit()
+
     def test_load_sample_direct(self, db):
         """Test a low-level load and commit of this database object"""
         SampleSchema = db.interface.sample()
+        assert SampleSchema.opts.model is db.model.sample
         res = SampleSchema.load(test_sample, session=db.session)
         db.session.add(res)
         db.session.commit()
