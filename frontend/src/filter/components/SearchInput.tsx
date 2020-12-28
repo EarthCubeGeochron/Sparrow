0209cd957dd68d318c51cfc257b8f7b78bd47e75
Casey Idzikowski <idzikoj1234@gmail.com>
2020-12-28 16:28:43 -0600

    Root Search Component, replace filterbox

---
+++ frontend/src/filter/components/SearchInput.tsx
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+import h from "@macrostrat/hyper";
+import { InputGroup } from "@blueprintjs/core";
+
+export function SearchInput(props) {
+  const [text, setText] = useState("");
+
+  const handleChange = (e) => {
+    setText(e.target.value);
+  };
+
+  return h(InputGroup, {
+    leftIcon: "search",
+    placeholder: "Search for anything...",
+    value: text,
+    onChange: handleChange,
+  });
+}
