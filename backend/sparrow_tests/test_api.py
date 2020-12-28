1450727ce47bb3938e46255d2d51ac265d4d8bde
Casey Idzikowski <idzikoj1234@gmail.com>
2020-12-28 12:24:23 -0600

    API filter testing

--- backend/sparrow_tests/test_api.py
+++ backend/sparrow_tests/test_api.py
@@ -12,7 +12,7 @@ def create_location_from_coordinates(longitude, latitude):
     '''This function will create the json-like object 
          in database from long & lat given in a post request
     '''
-    location = from_shape(Point(longitude, latitude), srid=4346)
+    location = from_shape(Point(longitude, latitude), srid=4326)
     return location
 
 class TestAPIV2:
@@ -42,75 +42,7 @@ class TestAPIV2:
         res = client.get(route)
         assert res.status_code == 200
 
-
-    def test_api_datasheet(self, client, db):
-        """
-        Test to go along with the datasheet editor api plugin.
-
-        in the database, material has a foreign key constraint, vocabulary.material.id (which is the name, i.e basalt)
-        Polymorhpic subclass? 
-
-        some helpful session func:
-            session.new : shows pending objects in a session
-            session.dity : shows modifications to persistant objects
-            session.commit() : commits everything in session que
-            session.add/add_all: pass a class and values ex: Users(firstname='Casey')
-        
-        Remember that session doesn't connect to database until specified to do so.
-        on a query if you don't call a chain method (i.e .one(), .first(), .all()) you will
-        only have a query object.
-
-        """
-        ## first load material data and then sample data so i can have material in the sample
-        Material =db.model.vocabulary_material
-        Sample = db.model.sample
-
-        db.session.add_all([
-            Material(id='basalt'),
-            Material(id='dacite'),
-            Material(id='lava'),
-        ])
-
-        #db.session.commit()
-
-        db.session.add_all([
-            Sample(id=18, name='M2C', material='basalt', location = create_location_from_coordinates(120,80)),
-            Sample(id=19, name='Test_sample', material='dacite', location = create_location_from_coordinates(60,30)),
-            Sample(id=20, name='Test_sample2', material='lava', location = create_location_from_coordinates(170,34)),
-        ])
-
-        db.session.commit()
-
-        res_test = client.get("/api/v2/models/sample?per_page=5")
-        test_sample_data = res_test.json()
-        assert res_test.status_code == 200
-
-        ## Check if data went through, both material and sample data would have to have gone through
-        assert len(test_sample_data['data']) == 3 
-
-        edited_sample_data = json.dumps([{
-                "name": "M2C_add",
-                "material": "dacite",
-                "id":18
-                }, 
-                {
-                    "name":"Test_add", 
-                    "material": "This_doesn't_exist", 
-                    "id":19,
-                    "latitude": 32,
-                    "longitude": 120
-                    }
-                ])
-
-        route = '/api/v2/datasheet/edits'
-        data = {"Status": "Success"}
-        res = client.post(route, json=edited_sample_data)
-        assert res.status_code == 200
-        assert res.json() == data
-
-
-    def test_api_datasheet_view(self,db, client):
-        ## first load material data and then sample data so i can have material in the sample
+    def test_load_data(self, client, db):
         Material =db.model.vocabulary_material
         Sample = db.model.sample
 
@@ -130,6 +62,30 @@ class TestAPIV2:
         ])
 
         db.session.commit()
+        pass
+
+
+    def test_api_datasheet(self, client, db):
+        """
+        Test to go along with the datasheet editor api plugin.
+        """
+        res_test = client.get("/api/v2/models/sample?per_page=5")
+        test_sample_data = res_test.json()
+        assert res_test.status_code == 200
+
+        ## Check if data went through, both material and sample data would have to have gone through
+        assert len(test_sample_data['data']) != 0 
+
+        edited_sample_data = json_fixture("sample-edits.json")
+
+        route = '/api/v2/datasheet/edits'
+        data = {"Status": "Success"}
+        res = client.post(route, json=edited_sample_data)
+        assert res.status_code == 200
+        assert res.json() == data
+
+    def test_api_datasheet_view(self,db, client):
+        ## first load material data and then sample data so i can have material in the sample
 
         route = 'api/v2/datasheet/view'
         res = client.get(route)
@@ -147,24 +103,3 @@ class TestAPIV2:
         data = res.json()
         assert data["total_count"] == 1 ## it fails here because theres no data. db.load_data isn't working
         assert data["data"][0]["name"] == "Declarative import test"
-
-    def test_date_filter(self, client, db):
-        """Testing the date_range filter on the api"""
-
-        Session = db.model.session
-
-        date = datetime.datetime(2013, 3, 12)
-        date1 = datetime.datetime(2013, 6, 1)
-        date2 = datetime.datetime(2014, 6, 3)
-
-        db.session.add_all([
-            Session(date=date),
-            Session(date=date1),
-            Session(date=date2),
-        ])
-        db.session.commit()
-
-        # %Y-%m-%d/%H:%M:%S
-        response = client.get("/api/v2/models/session?date_range=2012-01-12/00:00:00,2015-01-01/00:00:00")
-
-        assert len(response.json()['data']) == 3
---
+++ backend/sparrow_tests/test_api_filters.py
@@ -0,0 +1,86 @@
+import json
+from pytest import mark
+from .fixtures import basic_data
+from .helpers import json_fixture
+from geoalchemy2.shape import from_shape, to_shape
+from shapely.geometry import mapping, Point
+import datetime
+import pdb
+
+def create_location_from_coordinates(longitude, latitude):
+    '''This function will create the json-like object 
+         in database from long & lat given in a post request
+    '''
+    location = from_shape(Point(longitude, latitude), srid=4326)
+    return location
+
+class TestAPIV2_filters:
+    def test_load_data(self, client, db):
+        Material =db.model.vocabulary_material
+        Sample = db.model.sample
+
+        db.session.add_all([
+            Material(id='basalt'),
+            Material(id='dacite'),
+            Material(id='lava'),
+        ])
+
+        db.session.commit()
+
+        db.session.add_all([
+            Sample(id=18, name='M2C', material='basalt', location = create_location_from_coordinates(120,80)),
+            Sample(id=19, name='Test_sample', material='dacite', location = create_location_from_coordinates(60,30)),
+            Sample(id=20, name='Test_sample2', material='lava', location = create_location_from_coordinates(170,34)),
+            Sample(id=21, name='Test_sample3', material='lava'),
+        ])
+
+        db.session.commit()
+
+    def test_date_filter(self, client, db):
+        """Testing the date_range filter on the api"""
+
+        Session = db.model.session
+
+        date = datetime.datetime(2013, 3, 12)
+        date1 = datetime.datetime(2013, 6, 1)
+        date2 = datetime.datetime(2014, 6, 3)
+
+        db.session.add_all([
+            Session(date=date),
+            Session(date=date1),
+            Session(date=date2),
+        ])
+        db.session.commit()
+
+        # %Y-%m-%d/%H:%M:%S
+        response = client.get("/api/v2/models/session?date_range=2012-01-12,2015-01-01")
+
+        assert len(response.json()['data']) == 3
+
+    def test_location_filters(self, client, db):
+        """
+        Testing the coordinate filter and the WKT geometry Filter
+        """
+
+        coord_response = client.get("/api/v2/models/sample?coordinates=-180,-90,180,90")
+        coord_data = coord_response.json()
+        assert coord_data['total_count'] != 0
+
+        geometry_response = client.get("/api/v2/models/sample?geometry=POLYGON((0 0 ,180 90, 0 90, 180 0, 0 0))")
+        geom_data = geometry_response.json()
+
+        assert geom_data['total_count'] != 0
+
+    def test_doi_filter(self, client, db):
+        data = json_fixture("project-doi-fixes.json")
+        db.load_data("project", data["data"])
+
+        pub_res = client.get("/api/v2/models/publication?doi_like=10.10")
+        pub_json = pub_res.json()
+        assert pub_json['total_count'] != 0
+
+
+        proj_res = client.get("api/v2/models/project?doi_like=10.10")
+        proj_json = proj_res.json()
+        assert proj_json['total_count'] != 0
+
