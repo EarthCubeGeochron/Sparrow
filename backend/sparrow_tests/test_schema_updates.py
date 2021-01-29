2109ea24bbdfff60b6d141247f192a5d21e059a5
Daven Quinn <dev@davenquinn.com>
2021-01-29 03:29:58 -0600

    Added another test that will fail

--- backend/sparrow_tests/test_schema_updates.py
+++ backend/sparrow_tests/test_schema_updates.py
@@ -12,7 +12,6 @@ test_sample = {
 
 
 class TestSchemaUpdates:
-    @mark.skip
     def test_load_sample_imperative(self, db):
         """A simple test to make sure we can load a single sample with
         a linked material"""
@@ -28,23 +27,23 @@ class TestSchemaUpdates:
         db.session.add(obj)
         db.session.commit()
 
-    @mark.xfail(reason="We haven't figured this out yet")
     def test_load_sample_direct(self, caplog, db):
         """Test a low-level load and commit of this database object"""
         caplog.set_level(logging.DEBUG, logger="marshmallow")
         SampleSchema = model_interface(db.model.sample, session=db.session)()
         res = SampleSchema.load(test_sample, session=db.session)
         assert isinstance(res, db.model.sample)
+
         assert (
             SampleSchema.fields["_material"].related_model
             is db.model.vocabulary_material
         )
         assert isinstance(res._material, db.model.vocabulary_material)
+        assert id(db.model.vocabulary_material) == id(type(res._material))
 
         db.session.add(res)
         db.session.commit()
 
-    @mark.xfail(reason="We haven't figured this out yet")
     def test_load_sample_highlevel(self, db):
         """Test a higher-level commit using our load-data infrastructure.
         If this fails while the previous test succeeds, it is likely an
@@ -54,7 +53,6 @@ class TestSchemaUpdates:
         test_sample["name"] = "a8fa3ac"
         db.load_data("sample", test_sample)
 
-    @mark.xfail(reason="We haven't figured this out yet")
     def test_retrieve_sample(self, db):
         obj = db.session.query(db.model.sample).filter_by(name="4c1321a").one()
         assert obj._material.id == "Granite"
