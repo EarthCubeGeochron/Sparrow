37e527410de9874ed373752acdd7fb0524de41cb
Daven Quinn <dev@davenquinn.com>
2021-01-29 02:56:10 -0600

    Added a test of whether a migration is needed

--- backend/sparrow/database/migration.py
+++ backend/sparrow/database/migration.py
@@ -20,8 +20,7 @@ def temp_database(conn_string):
         drop_database(engine.url)
 
 
-def db_migration(db, safe=True, apply=False):
-    """Create a database migration against the idealized schema"""
+def create_migration(db, safe=True):
     url = "postgres://postgres@db:5432/sparrow_temp_migration"
     with redirect_stdout(sys.stderr):
         with temp_database(url) as engine:
@@ -35,6 +34,17 @@ def db_migration(db, safe=True, apply=False):
             m.set_safety(safe)
             # Not sure what this does
             m.add_all_changes()
+    return m
+
+
+def needs_migration(db):
+    migration = create_migration(db)
+    return len(migration.statements) == 0
+
+
+def db_migration(db, safe=True, apply=False):
+    """Create a database migration against the idealized schema"""
+    m = create_migration(db, safe=safe)
     print("===MIGRATION BELOW THIS LINE===", file=sys.stderr)
     for s in m.statements:
         if apply:
