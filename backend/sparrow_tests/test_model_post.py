b55c78f5f0070ff98ad9334b4fae3535925e5eb1
Casey Idzikowski <idzikoj1234@gmail.com>
2021-02-17 17:52:36 -0500

    post and put tests

--- backend/sparrow_tests/test_model_post.py
+++ backend/sparrow_tests/test_model_post.py
@@ -4,6 +4,8 @@ from urllib.request import urlopen as uReq
 
 from .helpers import json_fixture
 import json
+import sys
+from datetime import datetime
 
 
 def create_BibJSON(crossref_object,doi):
@@ -74,8 +76,57 @@ class TestModelPost:
 
         assert len(res_json['data']) > 0
 
+    
+    def test_add_project_from_existing(self, db, client):
+        route = "/api/v2/models/project"
 
-        #db.load_data("project", data
+        new_project = json_fixture("new-project.json")
+
+        res = client.post(route, json=new_project)
+
+        data = res.json()
+
+        assert 0 == 1
+
+    def test_edit_researcher_in_project(self,db,client):
+        route = "/api/v2/models/project/1" ## editing the first researcher
+
+        edits = {"researchers": [{"name": "casey"}]}
+        res_put = client.put(route, json = edits)
+        assert res_put.status_code == 200
+
+    
+    def test_verylarge_import(self, client):
+
+        import_size = 1e7
+        i = 0
+        analysis = []
+        size = sys.getsizeof(analysis)
+        for i in range(0,2):
+            item = {
+                            "analysis_type": "Stable isotope analysis",
+                            "session_index": i,
+                            "datum": [
+                                {
+                                    "value": 0.2,
+                                    "error": 0.035,
+                                    "type": {"parameter": "delta 13C", "unit": "permille"},
+                                }
+                            ],
+                        }
+            analysis.append(item)
+        
+        data = {
+                    "date": str(datetime.now()),
+                    "name": "Session with existing instances",
+                    "sample": {"name": "LargeImport"},
+                    "analysis": analysis
+        }
+        res = client.put("/api/v1/import-data/session", json=json.dumps(data))
+
+        assert 0 == 1
+        assert res.status_code == 201
+
 
     def test_webscrape_app_sims(self,db, client):
         '''
@@ -93,19 +144,27 @@ class TestModelPost:
 
         content = page_soup.findAll('p', {'class' : 'item article'})
         title_list = []
+        doi_list = []
         for pub in content:
             title = pub.findAll('span', {"class": "body"})[0].text
             title_list.append(title)
 
+            doi = pub.findAll('span', {"class": "doi"})[0].text
+            doi_list.append(doi)
+
         proj_titles = []
-        for title in title_list:
-            proj_titles.append({"name":title})
+        for i, title in enumerate(title_list):
+            proj_titles.append({"name":title, "publications" :[{"title":title, "doi": doi_list[i]}]})
 
 
         res = client.post(route, json= proj_titles)
 
         up_json = res.json()
         assert len(up_json['data']) > 0
+        assert 0 ==1
+
+
+    
 
     # def test_webscrape_publications_sims(self,db,client):
     #     '''
