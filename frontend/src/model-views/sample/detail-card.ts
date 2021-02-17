3b2242b0e504036b234a8035134416fbc4d00bd8
Casey Idzikowski <idzikoj1234@gmail.com>
2021-02-17 18:11:20 -0500

    Model Edit Cards, easy onclick props

--- frontend/src/model-views/sample/detail-card.ts
+++ frontend/src/model-views/sample/detail-card.ts
@@ -1,5 +1,5 @@
 import { hyperStyled } from "@macrostrat/hyper";
-import { Card, Icon } from "@blueprintjs/core";
+import { Card, Icon, Button, ButtonGroup } from "@blueprintjs/core";
 import { LinkCard } from "@macrostrat/ui-components";
 import style from "./card.module.css";
 import styles from "./module.styl";
@@ -14,16 +14,14 @@ interface SampleCardProps {
   material: string;
   location_name?: string;
   setID?: (any) => {};
+  onClick?: (any) => {};
 }
 /**
  *
  * @param props : name (string), id (number), link (boolean), material (string), location_name? (string)
  */
 const SampleCard = function(props: SampleCardProps) {
-  let { material, id, name, location_name, link, setID } = props;
-  if (link == null) {
-    link = true;
-  }
+  let { material, id, name, location_name, link = true, setID } = props;
 
   const onHover = () => {
     //set id to state so marker is highlighted
@@ -35,7 +33,7 @@ const SampleCard = function(props: SampleCardProps) {
     setID(null);
   };
 
-  const component = link != null ? LinkCard : Card;
+  const component = link ? LinkCard : Card;
   const to = useModelURL(`/sample/${id}`);
   return h(
     component,
@@ -53,6 +51,52 @@ const SampleCard = function(props: SampleCardProps) {
   );
 };
 
+/**
+ * Component to allow for deletion of samples from Project.
+ * @param props
+ */
+const SampleEditCard = (props) => {
+  let { id, name, onClick } = props;
+
+  return h("div.sample-edit-card", [
+    h("h4.name", name),
+    h(Button, {
+      minimal: true,
+      icon: "trash",
+      intent: "danger",
+      onClick: () => onClick(id),
+    }),
+  ]);
+};
+
+const PubEditCard = (props) => {
+  let { id, content, onClick } = props;
+
+  return h("div.pub-edit-card", [
+    content,
+    h(Button, {
+      minimal: true,
+      icon: "trash",
+      intent: "danger",
+      onClick: () => onClick(id),
+    }),
+  ]);
+};
+
+const ResearcherEditCard = (props) => {
+  let { id, name, onClick } = props;
+
+  return h("div.sample-edit-card", [
+    h("h4.name", name),
+    h(Button, {
+      minimal: true,
+      icon: "trash",
+      intent: "danger",
+      onClick: () => onClick(id),
+    }),
+  ]);
+};
+
 interface AddSample {
   icon_name: string;
   onClick: () => {};
@@ -63,4 +107,10 @@ const AddSampleCard = (props: AddSample) => {
   return h("div", { className: style.addSample, onClick }, ["+"]);
 };
 
-export { SampleCard, AddSampleCard };
+export {
+  SampleCard,
+  AddSampleCard,
+  SampleEditCard,
+  PubEditCard,
+  ResearcherEditCard,
+};
