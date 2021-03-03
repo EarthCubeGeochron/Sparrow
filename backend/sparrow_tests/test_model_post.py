6d0a0c9d5998f3f284a7bbb9d5f2e93d9e21ee19
Casey Idzikowski <idzikoj1234@gmail.com>
2021-03-03 11:25:44 -0600

    python formatting

--- backend/sparrow_tests/test_model_post.py
+++ backend/sparrow_tests/test_model_post.py
@@ -7,6 +7,7 @@ import json
 import sys
 from datetime import datetime
 
+
 class TestModelPost:
     """
     Testing Suite to test the universal POST method on the api
@@ -24,7 +25,7 @@ class TestModelPost:
 
         res_json = res.json()
 
-        #assert len(res_json["data"]) > 0
+        # assert len(res_json["data"]) > 0
 
     def test_add_project_from_existing(self, db, client):
         route = "/api/v2/models/project"
@@ -35,34 +36,31 @@ class TestModelPost:
 
         data = res.json()
 
-    def test_edit_researcher_in_project(self,db,client):
-        route = "/api/v2/models/project/1" ## editing the first researcher
+    def test_edit_researcher_in_project(self, db, client):
+        route = "/api/v2/models/project/1"  ## editing the first researcher
 
         edits = {"researcher": [{"name": "casey", "orcid": None}]}
-        res_put = client.put(route, json = edits)
+        res_put = client.put(route, json=edits)
 
-       # assert res_put.status_code == 200
+    # assert res_put.status_code == 200
 
+    def test_webscrape_app_sims(self, db, client):
+        """
+        This test webscrapes an html page, taken from online.
 
-    def test_webscrape_app_sims(self,db, client):
-        '''
-            This test webscrapes an html page, taken from online.
+            # sims_pub_url = "http://www.geology.wisc.edu/~wiscsims/publications.html"
 
-                # sims_pub_url = "http://www.geology.wisc.edu/~wiscsims/publications.html"
+        This can also be achieved by accessing the html through a network connection directly.
 
-            This can also be achieved by accessing the html through a network connection directly.
+            # from urllib.request import urlopen as uReqfrom
+            #
+            # page = uReq(sims_pub_url)
+            # page_html = page.read()
+            # page.close()
 
-                # from urllib.request import urlopen as uReqfrom 
-                #
-                # page = uReq(sims_pub_url)
-                # page_html = page.read()
-                # page.close()
-                
-        '''
+        """
         route = "/api/v2/models/project"
 
-
-         
         page = Path(relative_path(__file__, "fixtures/wiscsims_publications.html"))
         page_open = open(page, "r")
         page_html = page_open.read()
@@ -70,22 +68,21 @@ class TestModelPost:
 
         page_soup = soup(page_html, "html.parser")
 
-        content = page_soup.findAll('p', {'class' : 'item article'})
+        content = page_soup.findAll("p", {"class": "item article"})
         title_list = []
         doi_list = []
         for pub in content:
-            title = pub.findAll('span', {"class": "body"})[0].text
+            title = pub.findAll("span", {"class": "body"})[0].text
             title_list.append(title)
 
-            doi = pub.findAll('span', {"class": "doi"})[0].text
+            doi = pub.findAll("span", {"class": "doi"})[0].text
             doi_list.append(doi)
 
         proj_titles = []
         for i, title in enumerate(title_list):
-            proj_titles.append({"name":title, "publications" :[{"title":title, "doi": doi_list[i]}]})
+            proj_titles.append({"name": title, "publications": [{"title": title, "doi": doi_list[i]}]})
 
-
-        res = client.post(route, json= proj_titles)
+        res = client.post(route, json=proj_titles)
 
         up_json = res.json()
-        assert len(up_json['data']) > 0
+        assert len(up_json["data"]) > 0
