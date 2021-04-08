9819c2ae97c53952bf19b80e6836f9ccb37b5fc7
Casey Idzikowski <idzikoj1234@gmail.com>
2021-04-08 13:10:16 -0500

    tags testing

--- backend/sparrow_tests/test_tags.py
+++ backend/sparrow_tests/test_tags.py
@@ -18,6 +18,7 @@ class TestTags:
         assert len(d["data"]) >= 3
 
     def test_post_tag(self, client, db):
+        '''Create a new tag add to db and make sure it was entered'''
         data = json_fixture("tags.json")
         tags = db.model.tags_tag
 
@@ -28,8 +29,8 @@ class TestTags:
         q = db.session.query(tags).filter(tags.name == new_tag["name"]).first()
         assert q.name == new_tag["name"]
 
-    def test_add_tag_to_datum(self, client, db):
-        """create a new datum and then add a tag to it"""
+    def test_add_tag_to_model(self, client, db):
+        """create a new sample and then add a tag to it"""
 
         sample = db.model.sample
         tags = db.model.tags_tag
@@ -57,3 +58,43 @@ class TestTags:
 
         s = db.session.query(sample).get(sample_id)
         assert len(s.tags_tag_collection) != 0
+    
+    def test_remove_tag_from_model(self, client, db):
+        '''Remove a tag from the model created above'''
+        data = json_fixture("tags.json")
+
+        new_sample = data['new_sample']
+        tag_to_remove = data['new_tag']
+
+        sample = db.model.sample
+        tags = db.model.tags_tag
+
+        s = db.session.query(sample).filter(sample.name == new_sample['name']).first()
+        t = db.session.query(tags).filter(tags.color == tag_to_remove['color']).first()
+
+        assert len(s.tags_tag_collection) > 0
+        assert t.name == tag_to_remove['name']
+
+        tag_id = t.id ## unqiue id assigned in database
+
+        new_tag_collection = []
+        for tag in s.tags_tag_collection:
+            if tag.id != tag_id:
+                new_tag_collection.append(tag)
+        
+        s.tags_tag_collection = new_tag_collection
+        assert len(db.session.dirty) == 1 # we've changed the tag_collection
+        try:
+            db.session.commit()
+        except:
+            db.session.rollback()
+        
+        s = db.session.query(sample).filter(sample.name == new_sample['name']).first()
+        exist_col = []
+        for tag in s.tags_tag_collection:
+            if tag.id == tag_id:
+                exist_col.append(tag)
+        
+        assert len(exist_col) == 0
+
+
