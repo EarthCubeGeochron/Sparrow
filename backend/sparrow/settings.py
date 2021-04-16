b97b1402f2f7752d131f10b7eeae71c178eca429
Casey Idzikowski <idzikoj1234@gmail.com>
2021-04-16 15:48:26 -0400

    cast bool from env

--- backend/sparrow/settings.py
+++ backend/sparrow/settings.py
@@ -2,12 +2,10 @@ from starlette.config import Config
 
 config = Config()
 
-echo = config("SPARROW_ECHO_SQL", default="false").lower() in ["true"] 
-
 LAB_NAME = config("SPARROW_LAB_NAME", default="Test lab")
 DATABASE = config("SPARROW_DATABASE", default="postgresql+psycopg2:///sparrow")
 BASE_URL = config("SPARROW_BASE_URL", default="/")
-ECHO_SQL = echo
+ECHO_SQL = config("SPARROW_ECHO_SQL", cast=bool, default=False)
 
 SECRET_KEY = config("SPARROW_SECRET_KEY", None)
 if SECRET_KEY is None:
