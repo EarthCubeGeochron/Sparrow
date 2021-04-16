415a1a879ee0b6dc1f80c6a029eb06aa846dc6e2
Casey Idzikowski <idzikoj1234@gmail.com>
2021-04-16 10:48:06 -0400

    ECHO_SQL env variable

--- backend/sparrow/logs/config.py
+++ backend/sparrow/logs/config.py
@@ -1,3 +1,5 @@
+from sparrow.settings import ECHO_SQL
+
 include_server_header = False
 workers = 1
 
@@ -35,7 +37,7 @@ logconfig_dict = {
         "sqlalchemy.engine": {
             "handlers": ["default"],
             "level": "INFO",
-            "propagate": False,
+            "propagate": ECHO_SQL,
         },
         "__main__": {  # if __name__ == '__main__'
             "handlers": ["error_console"],
