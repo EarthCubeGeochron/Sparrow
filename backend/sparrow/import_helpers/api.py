82740f211827a2df6fde92657c627c73341b5a44
Daven Quinn <dev@davenquinn.com>
2021-04-30 01:33:22 -0500

    Websocket now works

--- backend/sparrow/import_helpers/api.py
+++ backend/sparrow/import_helpers/api.py
@@ -1,23 +1,24 @@
-from starlette.websockets import WebSocket
 from asyncio import sleep
 from sparrow.plugins import SparrowCorePlugin
 
 
-async def app(scope, receive, send):
-    websocket = WebSocket(scope=scope, receive=receive, send=send)
-    await websocket.accept()
-    counter = 0
+async def websocket_route(session):
+    await session.accept()
+    counter = 1
     while True:
-        await websocket.send_text(f"Hello, planet {counter}!")
-        await sleep(2)
+        await session.send_text(f"Hello, planet {counter}!")
+        await sleep(1)
         counter += 1
-    await websocket.close()
+    await session.close()
 
 
 class ImportTrackerPlugin(SparrowCorePlugin):
     name = "import-tracker"
 
     def on_api_initialized_v2(self, api):
-        api.mount(
-            "/import-tracker", app, name="import_tracker_api", help="Websocket server from tracking import process"
+        api.add_websocket_route(
+            "/import-tracker",
+            websocket_route,
+            name="import_tracker_api",
+            # help="Websocket server from tracking import process",
         )
