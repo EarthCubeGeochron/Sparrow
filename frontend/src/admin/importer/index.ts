c0396c1b791e6b25445da891b094f70eed6e4355
Daven Quinn <dev@davenquinn.com>
2021-04-29 23:29:46 -0500

    WIP attempt to fix websockets passthrough

--- frontend/src/admin/importer/index.ts
+++ frontend/src/admin/importer/index.ts
@@ -1,5 +1,5 @@
 import h from "@macrostrat/hyper";
-import { useRef, useMemo } from "react";
+import { useRef, useMemo, useEffect } from "react";
 import { useAPIHelpers } from "@macrostrat/ui-components";
 import { APIV2Context } from "~/api-v2";
 import useWebSocket, { ReadyState } from "react-use-websocket";
@@ -7,21 +7,36 @@ import useWebSocket, { ReadyState } from "react-use-websocket";
 function ImporterMain() {
   const helpers = useAPIHelpers(APIV2Context);
   const url1 = helpers.buildURL("/import-tracker");
-  const url = "wss://localhost:5002/api/v2/import-tracker";
+  const url = "ws://localhost:5002/api/v2/import-tracker";
   console.log(url);
 
-  const { sendMessage, lastMessage, readyState } = useWebSocket(url);
+  const { sendMessage, lastMessage, readyState } = useWebSocket(url, {
+    onOpen: () => console.log("opened"),
+  });
   const messageHistory = useRef([]);
+
+  const connectionStatus = {
+    [ReadyState.CONNECTING]: "Connecting",
+    [ReadyState.OPEN]: "Open",
+    [ReadyState.CLOSING]: "Closing",
+    [ReadyState.CLOSED]: "Closed",
+    [ReadyState.UNINSTANTIATED]: "Uninstantiated",
+  }[readyState];
 
   messageHistory.current = useMemo(
     () => messageHistory.current.concat(lastMessage),
     [lastMessage]
   );
 
-  return h(
-    "div",
-    messageHistory.current.map((d) => `${d}`)
-  );
+  useEffect(() => sendMessage("Hello"), []);
+
+  return h("div", [
+    h("div.status", "WebSocket connection: " + connectionStatus),
+    h(
+      "div.message-history",
+      messageHistory.current.map((d) => `${d}`)
+    ),
+  ]);
 }
 
 function ImporterPage() {
