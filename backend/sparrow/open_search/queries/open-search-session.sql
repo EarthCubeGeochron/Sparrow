bd2070f233a1931f322b96f0a6cded98aedbe5b5
Casey Idzikowski <idzikoj1234@gmail.com>
2021-05-18 21:24:25 -0500

    fixed json issues

--- backend/sparrow/open_search/queries/open-search-session.sql
+++ backend/sparrow/open_search/queries/open-search-session.sql
@@ -5,7 +5,7 @@ WITH search AS (
     SELECT to_tsquery(string_agg(lexeme || ':*', ' & ' order by positions)) AS query
     FROM unnest(to_tsvector(:query))
 )
-SELECT DISTINCT ss.*
+SELECT DISTINCT ss.id, ss.date, ss.date_precision, ss.end_date,ss.instrument, ss.technique, ss.target, ss.embargo_date, ss.data
 FROM search, session ss
 LEFT JOIN documents.session_document ssd
 ON ssd.session_id=ss.id
