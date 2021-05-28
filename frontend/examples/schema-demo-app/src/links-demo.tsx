d956930f4c8a7f9730e539fbe596ac362f69b9de
Daven Quinn <dev@davenquinn.com>
2021-05-27 20:12:18 -0500

    Pretty nice nested levels

--- frontend/examples/schema-demo-app/src/links-demo.tsx
+++ frontend/examples/schema-demo-app/src/links-demo.tsx
@@ -5,6 +5,7 @@ import { LinearGradient } from "@visx/gradient";
 import { LinkHorizontal } from "@visx/shape";
 import { pointRadial } from "d3-shape";
 import useDimensions from "react-use-dimensions";
+import h from "@macrostrat/hyper";
 
 function useForceUpdate() {
   const [, setValue] = useState<number>(0);
@@ -18,7 +19,7 @@ interface TreeNode {
 }
 
 const data: TreeNode = {
-  name: "Sparrow User API",
+  name: "hidden_root",
   children: [
     {
       name: "Concordia parameters",
@@ -58,18 +59,85 @@ const data: TreeNode = {
   ],
 };
 
-const defaultMargin = { top: 30, left: 30, right: 30, bottom: 70 };
+const defaultMargin = { top: 30, left: 70, right: 70, bottom: 30 };
 
 export type LinkTypesProps = {
   width: number;
   height: number;
   margin?: { top: number; right: number; bottom: number; left: number };
+  hideRoot: boolean;
 };
 
-export default function Example({ margin = defaultMargin }: LinkTypesProps) {
-  const [stepPercent, setStepPercent] = useState<number>(0.5);
-  const [ref, { width: totalWidth, height: totalHeight }] = useDimensions();
+function renderLink(link, i) {
+  if (link.source.data.name == "hidden_root") return null;
+  return h(LinkHorizontal, {
+    key: i,
+    data: link,
+    stroke: "#4af2a1",
+    strokeWidth: 2,
+    fill: "none",
+  });
+}
+
+function Node({ width, height, node }) {
   const forceUpdate = useForceUpdate();
+  return h("rect", {
+    height: height,
+    width: width,
+    y: -height / 2,
+    x: -width / 2,
+    fill: "#ffffff",
+    stroke: "#888888",
+    strokeWidth: 1,
+    strokeDasharray: node.data.children ? "0" : "2,2",
+    strokeOpacity: node.data.children ? 1 : 0.6,
+    rx: node.data.children ? 0 : 4,
+    onClick: () => {
+      node.data.isExpanded = !node.data.isExpanded;
+      console.log(node);
+      forceUpdate();
+    },
+  });
+}
+
+function renderNodes(node, key) {
+  const width = 80;
+  const height = 30;
+  let top = node.x;
+  let left = node.y;
+  if (node.depth === 0) return null;
+  return h(
+    Group,
+    {
+      top: top,
+      left: left,
+      key: key,
+    },
+    [
+      h(Node, { width, height, node }),
+      h(
+        "text",
+        {
+          dy: ".33em",
+          fontSize: 9,
+          fontFamily: "Arial",
+          textAnchor: "middle",
+          style: {
+            pointerEvents: "none",
+          },
+          fill: "black",
+        },
+        node.data.name
+      ),
+    ]
+  );
+}
+
+export default function Example({
+  margin = defaultMargin,
+  hideRoot = false,
+}: LinkTypesProps) {
+  const [ref, { width: totalWidth, height: totalHeight }] = useDimensions();
 
   const innerWidth = totalWidth - margin.left - margin.right;
   const innerHeight = totalHeight - margin.top - margin.bottom;
@@ -77,90 +145,30 @@ export default function Example({ margin = defaultMargin }: LinkTypesProps) {
   const sizeWidth = innerHeight;
   const sizeHeight = innerWidth;
 
-  return totalWidth < 10 ? null : (
+  if (totalWidth < 10) return null;
+
+  const treeRoot = hierarchy(data, (d) => (d.isExpanded ? null : d.children));
+  const nLevels = treeRoot.height - 1;
+
+  const widthIncludingHiddenRoot = sizeHeight / (nLevels / (nLevels + 1));
+
+  return (
     <div className="linker-ui-workspace" ref={ref}>
       <svg width={totalWidth} height={totalHeight}>
         <LinearGradient id="links-gradient" from="#cccccc" to="#dddddd" />
         <rect width={totalWidth} height={totalHeight} rx={14} fill="#dddddd" />
-        <Group top={margin.top} left={margin.left + sizeHeight}>
+        <Group top={margin.top} left={margin.left + widthIncludingHiddenRoot}>
           <Tree
-            root={hierarchy(data, (d) => (d.isExpanded ? null : d.children))}
-            size={[sizeWidth, -sizeHeight]}
-            separation={(a, b) => (a.parent === b.parent ? 1 : 0.5) / a.depth}
+            root={treeRoot}
+            size={[sizeWidth, -widthIncludingHiddenRoot]}
+            separation={(a, b) => (a.parent === b.parent ? 0.2 : 0.5) / a.depth}
           >
-            {(tree) => (
-              <Group top={origin.y} left={origin.x}>
-                {tree.links().map((link, i) => {
-                  console.log(link);
-                  return (
-                    <LinkHorizontal
-                      key={i}
-                      data={link}
-                      percent={stepPercent}
-                      stroke="#4af2a1"
-                      strokeWidth="2"
-                      fill="none"
-                    />
-                  );
-                })}
-
-                {tree.descendants().map((node, key) => {
-                  const width = 80;
-                  const height = 30;
-
-                  let top = node.x;
-                  let left = node.y;
-
-                  return (
-                    <Group top={top} left={left} key={key}>
-                      {node.depth === 0 && (
-                        <rect
-                          height={30}
-                          width={30}
-                          y={-15}
-                          x={-15}
-                          fill="salmon"
-                          onClick={() => {
-                            node.data.isExpanded = !node.data.isExpanded;
-                            console.log(node);
-                            forceUpdate();
-                          }}
-                        />
-                      )}
-                      {node.depth !== 0 && (
-                        <rect
-                          height={height}
-                          width={width}
-                          y={-height / 2}
-                          x={-width / 2}
-                          fill="#ffffff"
-                          stroke="#888888"
-                          strokeWidth={1}
-                          strokeDasharray={node.data.children ? "0" : "2,2"}
-                          strokeOpacity={node.data.children ? 1 : 0.6}
-                          rx={node.data.children ? 0 : 4}
-                          onClick={() => {
-                            node.data.isExpanded = !node.data.isExpanded;
-                            console.log(node);
-                            forceUpdate();
-                          }}
-                        />
-                      )}
-                      <text
-                        dy=".33em"
-                        fontSize={9}
-                        fontFamily="Arial"
-                        textAnchor="middle"
-                        style={{ pointerEvents: "none" }}
-                        fill="black"
-                      >
-                        {node.data.name}
-                      </text>
-                    </Group>
-                  );
-                })}
-              </Group>
-            )}
+            {(tree) => {
+              return h(Group, { top: origin.y, left: origin.x }, [
+                tree.links().map(renderLink),
+                tree.descendants().map(renderNodes),
+              ]);
+            }}
           </Tree>
         </Group>
       </svg>
