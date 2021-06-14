eb7bca690c8fee759e7b946db5660cd699343c54
Daven Quinn <dev@davenquinn.com>
2021-06-14 16:52:04 -0500

    A seemingly fairly complete user API

--- backend/sparrow/users/__init__.py
+++ backend/sparrow/users/__init__.py
@@ -2,8 +2,8 @@
 An API for managing users
 """
 from starlette.routing import Route, Router
-from starlette.exceptions import HTTPException
 from sparrow.plugins import SparrowCorePlugin
+from sparrow.database.models import User
 from sparrow.api import APIResponse, SparrowAPIError
 from sparrow.context import get_database
 from sparrow.auth.create_user import _create_user
@@ -12,15 +12,42 @@ from sparrow.logs import get_logger
 log = get_logger(__name__)
 
 
+def validate_password(password):
+    if len(password) < 4:
+        raise SparrowAPIError("Password is too short")
+    return password
+
+
 async def user(request):
     db = get_database()
     user_id = request.path_params["user_id"]
-    User = db.model.user
     UserSchema = db.interface.user
-    obj = db.session.query(User).get(user_id)
+    _schema = UserSchema(many=False, exclude=("password",))
+    with db.session_scope(commit=False):
+        user = db.session.query(User).get(user_id)
+        if request.method == "GET":
+            return APIResponse(user, schema=_schema)
+        elif request.method == "PUT":
+            # Update an existing user
+            data = await request.json()
+            password = data.get("password", None)
+            if password is not None:
+                user.set_password(validate_password(password))
 
-    if request.method == "GET":
-        return APIResponse(obj, schema=UserSchema(many=False, exclude=("password",)))
+            # We could probably use schemas nicely here, but we do things manually for now
+            for field in ("username", "researcher"):
+                try:
+                    setattr(user, field=data.get(field))
+                except KeyError:
+                    pass
+            db.session.add(user)
+            db.session.commit()
+            return APIResponse(user, schema=_schema)
+
+        elif request.method == "DELETE":
+            user.delete()
+            db.session.commit()
+            return APIResponse({"success": True, "deleted": [user_id]})
 
 
 async def create_user(db, User, request):
@@ -35,9 +62,7 @@ async def create_user(db, User, request):
         raise SparrowAPIError("Must provide both username and password")
     if UserSchema.query(db.session).get(username) is not None:
         raise SparrowAPIError(f"User {username} already exists", 409)
-    if len(password) < 4:
-        raise SparrowAPIError("Password is too short")
-    res = _create_user(db, username, password)
+    res = _create_user(db, username, validate_password(password))
     return APIResponse(res, schema=UserSchema(many=False, exclude=("password",)))
 
 
@@ -45,11 +70,12 @@ async def users(request):
     db = get_database()
     User = db.model.user
     UserSchema = db.interface.user
-    if request.method == "POST":
-        return create_user(db, UserSchema, request)
+    with db.session_scope(commit=False):
+        if request.method == "POST":
+            return create_user(db, UserSchema, request)
 
-    res = db.session.query(User).all()
-    return APIResponse(res, schema=UserSchema(many=True, exclude=("password",)))
+        res = db.session.query(User).all()
+        return APIResponse(res, schema=UserSchema(many=True, exclude=("password",)))
 
 
 UserAPI = Router(
