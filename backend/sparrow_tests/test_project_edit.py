669f881368d545566a6486d18244bac15d56bc96
Casey Idzikowski <idzikoj1234@gmail.com>
2021-06-16 16:33:45 -0400

    passing test using collection_handler func from api.endpoints.utils. Helps with adding collections to models

--- backend/sparrow_tests/test_project_edit.py
+++ backend/sparrow_tests/test_project_edit.py
@@ -1,7 +1,18 @@
+from sparrow.api.endpoints.utils import collection_handler
 from .helpers import json_fixture
 from pytest import mark, fixture
 import json
 
+def get_all_ids(db, model_name):
+    """ return all id's of model from database """
+
+    model = getattr(db.model, model_name)
+
+    models = db.session.query(model).all()
+
+    ids = [m.id for m in models]
+    return ids
+    
 
 class TestProjectEdits:
     """
@@ -61,18 +72,9 @@ class TestProjectEdits:
         # data["edit-project"].pop("id")
         updates = self.data["edit-project"]
 
-        # for k,v in updates.items():
-        #     if k in nests:
-        #         if type(v) is list:
-        #             for id in v:
-
-
-
         sessions = db.session.query(db.model.session).all()
         samples = db.session.query(db.model.sample).all()
         publications= db.session.query(db.model.publication).all()
-
-        assert 0 == 1
 
 
         # load updates into the project_schema and assign the same id as the existing
@@ -95,6 +97,65 @@ class TestProjectEdits:
         # it doesn't duplicate any other collection though.
         db.session.commit()
 
+        assert db.session.query(db.model.project).count() == 1
+
+        # the updates will have lengthened the publication collection
+        project_test = db.session.query(db.model.project).get(new_proj.id)
+        assert len(project_test.publication_collection) == 2
+        # adding a new sample to the session.sample attribute creates a new session..
+        sessions = db.session.query(db.model.session).all()
+        assert len(sessions) == 2
+
+    def test_remove_all_collections(self, db):
+
+        projects = db.session.query(db.model.project).all()
+
+        assert len(projects) == 1
+
+        project = projects[0]
+
+        project.session_collection = []
+        project.sample_collection = []
+        project.publication_collection = []
+
+        db.session.commit()
+
+        projects = db.session.query(db.model.project).all()
+        assert len(projects) == 1
+
+        assert len(projects[0].publication_collection) == 0
+
+    def test_collection_handler(self, db):
+        change_project_name = 'test change #3'
+        schema = db.interface.project()
+
+        projects = db.session.query(db.model.project).all()
+        assert len(projects) == 1
+
+        instance = projects[0]
+
+        data={'name':change_project_name}
+        data['sample']=get_all_ids(db, 'sample')
+        data['session'] = get_all_ids(db, 'session')
+        data['publication']=get_all_ids(db, 'publication')
+
+        assert len(data['sample']) == 3
+
+        data, model_collection = collection_handler(db, data, schema)
+        assert 'sample_collection' in  model_collection
+
+        new_proj = schema.load(data,session=db.session, instance=instance,partial=True)
+
+        for k,v in model_collection.items():
+            setattr(new_proj, k, v)
+
+        new_proj.id = instance.id
+        db.session.rollback()
+
+        db.session.merge(new_proj)
+
+        db.session.commit()
+        
         assert db.session.query(db.model.project).count() == 1
 
         # the updates will have lengthened the publication collection
