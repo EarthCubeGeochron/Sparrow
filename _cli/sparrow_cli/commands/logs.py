ef7347ddc0c553aa0f61c069f838e593e6f5c80e
Daven Quinn <dev@davenquinn.com>
2021-07-15 02:42:38 -0600

    More error handling for sparrow logs command

--- _cli/sparrow_cli/commands/logs.py
+++ _cli/sparrow_cli/commands/logs.py
@@ -5,11 +5,11 @@ from ..util import compose, container_id, cmd, log
 @click.command(name="logs")
 @click.argument("container", type=str, required=False, default=None)
 def sparrow_logs(container):
-    if container is None:
-        log.debug(f"Following logs for all containers")
-        compose("logs --tail=0 --follow")
-    else:
-        log.debug(f"Following logs for container {container}")
+    if container is not None and container.strip() != "":
         id = container_id(container)
         # bail if multiline string
-        cmd("docker logs -f", str(id))
+        if "\\n" not in id:
+            log.debug(f"Following logs for container {container}")
+            return cmd("docker logs -f", str(id))
+    log.debug(f"Following logs for all containers")
+    compose("logs --tail=0 --follow")
