e6843d04bf5f8cf6af139fe9d93924181c098fdf
Daven Quinn <dev@davenquinn.com>
2021-07-15 01:18:06 -0600

    Added logging

--- _cli/sparrow_cli/commands/logs.py
+++ _cli/sparrow_cli/commands/logs.py
@@ -1,14 +1,15 @@
 import click
-from ..util import compose, container_id, cmd
+from ..util import compose, container_id, cmd, log
 
 
 @click.command(name="logs")
 @click.argument("container", type=str, required=False, default=None)
 def sparrow_logs(container):
-    click.echo(container)
     if container is None:
+        log.debug(f"Following logs for all containers")
         compose("logs --tail=0 --follow")
     else:
+        log.debug(f"Following logs for container {container}")
         id = container_id(container)
         # bail if multiline string
         cmd("docker logs -f", str(id))
