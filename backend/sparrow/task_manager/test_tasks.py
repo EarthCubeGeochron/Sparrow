123f541e70a39bf63042a9728479d656aeac0d38
Daven Quinn <dev@davenquinn.com>
2021-08-11 05:14:23 -0400

    Improve task testing

--- backend/sparrow/task_manager/test_tasks.py
+++ backend/sparrow/task_manager/test_tasks.py
@@ -1,4 +1,20 @@
+from .. import task
+from click import echo
+
+
+@task(name="hello")
+def hello_task(name: str):
+    """Say hello!"""
+    text = f"Hello, {name}"
+    echo(text)
+    return text
+
+
 class TestSparrowTaskManager:
     def test_task_manager_exists(self, app):
         mgr = app.plugins.get("task-manager")
         assert mgr is not None
+        hello_task = mgr.get_task("hello")
+        assert hello_task is not None
+        assert hello_task._is_sparrow_task
+        assert hello_task("Sparrow") == "Hello, Sparrow"
