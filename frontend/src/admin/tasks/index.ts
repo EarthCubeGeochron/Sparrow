19551ed9346fb8ba5a0ace0a2c30dcb6f295ebaf
Daven Quinn <dev@davenquinn.com>
2021-08-18 15:20:26 -0500

    Some updates to tasks API

--- frontend/src/admin/tasks/index.ts
+++ frontend/src/admin/tasks/index.ts
@@ -4,10 +4,19 @@ import { ControlledSlider, useAPIHelpers } from "@macrostrat/ui-components";
 import { APIV2Context, useAPIv2Result } from "~/api-v2";
 import useWebSocket, { ReadyState } from "react-use-websocket";
 import { MinimalNavbar } from "~/components";
-import { Button, ButtonGroup, Menu, MenuItem } from "@blueprintjs/core";
+import {
+  Button,
+  ButtonGroup,
+  Menu,
+  MenuItem,
+  NonIdealState,
+} from "@blueprintjs/core";
 import styles from "./module.styl";
 import { FixedSizeList as List } from "react-window";
 const h = hyperStyled(styles);
+import { useParams, Switch, Route } from "react-router";
+import { Link } from "react-router-dom";
+import { useElementSize } from "@earthdata/sheet/src";
 
 const Row = ({ data, index, style }) => {
   return h(
@@ -21,21 +30,33 @@ const Row = ({ data, index, style }) => {
 
 const LogWindow = ({ messages }) => {
   const ref = useRef<List>();
+  const containerRef = useRef<HTMLElement>();
+  const extraLines = 2;
   useEffect(() => {
-    ref.current?.scrollToItem(messages.length - 1);
+    ref.current?.scrollToItem(messages.length + extraLines - 1);
   }, [messages.length]);
+
+  const { width, height } = useElementSize(containerRef, true) ?? {
+    width: 0,
+    height: 0,
+  };
+
   return h(
-    List,
-    {
-      ref,
-      height: 600,
-      itemCount: messages.length,
-      itemSize: 18,
-      itemData: messages,
-      width: 1200,
-      className: "message-history",
-    },
-    Row
+    "div.log-window",
+    { ref: containerRef },
+    h(
+      List,
+      {
+        ref,
+        height,
+        itemCount: messages.length + extraLines,
+        itemSize: 18,
+        itemData: messages,
+        width,
+        className: "message-history",
+      },
+      Row
+    )
   );
 };
 
@@ -47,7 +68,8 @@ const statusOptions = {
   [ReadyState.UNINSTANTIATED]: "Uninstantiated",
 };
 
-function ImporterMain({ task }) {
+function TaskMain({ task }) {
+  task = useParams().task;
   if (task == null) return null;
 
   const helpers = useAPIHelpers(APIV2Context);
@@ -83,7 +105,7 @@ function ImporterMain({ task }) {
     return messageHistory.current;
   }, [lastMessage]);
 
-  return h("div.importer-main", [
+  return h("div.tasks-main", [
     h(MinimalNavbar, { className: "navbar" }, [
       h("h3", task),
       h(ButtonGroup, { minimal: true }, [
@@ -107,35 +129,45 @@ function ImporterMain({ task }) {
   ]);
 }
 
-function TaskList({ tasks, selectedTask, onSelect }) {
-  return h("div.pipelines-list", [
-    h("h3", "Pipelines"),
+const TasksRouter = ({ base }) => {
+  return h(Switch, [
+    h(Route, {
+      path: base + "/:task",
+      render: () => h(TaskMain),
+    }),
+    h(Route, {
+      path: base,
+      render: () =>
+        h(NonIdealState, { message: "No task selected", icon: "warning" }),
+      exact: true,
+    }),
+  ]);
+};
+
+function TaskList({ tasks, base, task }) {
+  return h("div.tasks-list", [
+    h("h3", "Tasks"),
     h(
       Menu,
       (tasks ?? []).map((d) =>
         h(MenuItem, {
-          text: d.name,
-          active: selectedTask == d,
-          onClick() {
-            onSelect(d);
-          },
+          text: h(Link, { to: base + `/${d.name}` }, d.name),
+          selected: d.name === task,
+          disabled: d.name === task,
         })
       )
     ),
   ]);
 }
 
-function ImporterPage() {
+function TasksPage() {
   const tasks: any[] = useAPIv2Result("/tasks/")?.data ?? [];
-  const [task, setTask] = useState(null);
-  return h("div.importer-page", [
-    h(
-      "div.left-column",
-      null,
-      h(TaskList, { tasks, selectedTask: task, onSelect: setTask })
-    ),
-    h(ImporterMain, { task: task?.name }),
+  const task = useParams().task ?? null;
+  const base = "/admin/tasks";
+  return h("div.tasks-page", [
+    h("div.left-column", null, h(TaskList, { tasks, base, task })),
+    h(TasksRouter, { base }),
   ]);
 }
 
-export default ImporterPage;
+export default TasksPage;
