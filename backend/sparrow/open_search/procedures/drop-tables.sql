037d00f5c5b3f524f9f3750eba606970211359bc
Daven Quinn <dev@davenquinn.com>
2021-08-23 14:43:12 -0500

    A task to refresh search indices

---
+++ backend/sparrow/open_search/procedures/drop-tables.sql
@@ -0,0 +1,7 @@
+/*
+A procedure to run at initialization if the document tables are empty. 
+*/
+
+DROP TABLE documents.project_document;
+DROP TABLE documents.sample_document;
+DROP TABLE documents.session_document;
