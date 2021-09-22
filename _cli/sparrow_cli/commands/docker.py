12e767ce7c5894fb0dc6752d81a10ee03565d145
Idzikowski-Casey <Idzikowski-Casey@users.noreply.github.com>
2021-09-22 14:45:57 +0000

    Formatting changes

--- _cli/sparrow_cli/commands/docker.py
+++ _cli/sparrow_cli/commands/docker.py
@@ -2,19 +2,21 @@ import click
 from os import environ
 from subprocess import Popen
 
+
 def space(spaces=1):
-    for i in range(0,spaces):
+    for i in range(0, spaces):
         click.echo("")
+
 
 @click.command("system-prune")
 @click.option("--volume", default=False, help="prune sparrow volumes as well (bool)")
 def sparrow_docker_prune(volume):
     lab_name = environ.get("COMPOSE_PROJECT_NAME").lower()
     if len(lab_name) == 0:
-        click.secho("No compose project name environment variable set!", fg='red')
+        click.secho("No compose project name environment variable set!", fg="red")
     elif volume:
         space()
-        prompt1 = "This method will drop the data volumes including the database!" 
+        prompt1 = "This method will drop the data volumes including the database!"
         prompt2 = "If you don't want to loose data, backup the database first."
 
         click.secho(prompt1, fg="red")
@@ -25,19 +27,23 @@ def sparrow_docker_prune(volume):
         if ans == "I'm positive.":
             click.secho(f"Deleting volumes associated to {lab_name}", fg="yellow")
             filter_arg = f" | grep '{lab_name}*')"
-            volume_cmd = "docker volume rm $(docker volume ls --format '{{.Name}}'" + filter_arg
+            volume_cmd = (
+                "docker volume rm $(docker volume ls --format '{{.Name}}'" + filter_arg
+            )
             p = Popen(volume_cmd, shell=True)
             p.wait()
-            click.secho("Volumes deleted", fg='green')
+            click.secho("Volumes deleted", fg="green")
         else:
             click.secho("Good Choice", fg="green")
-            click.secho("Aborting...",fg='red')
-        
+            click.secho("Aborting...", fg="red")
+
     else:
         space()
-        click.secho(f"Removing images associated with {lab_name}", fg='yellow')
+        click.secho(f"Removing images associated with {lab_name}", fg="yellow")
         filter_arg = f" | grep '{lab_name}*')"
-        img_cmd = "docker image rm $(docker image ls --format '{{.Repository}}'" + filter_arg
+        img_cmd = (
+            "docker image rm $(docker image ls --format '{{.Repository}}'" + filter_arg
+        )
         p = Popen(img_cmd, shell=True)
         p.wait()
-        click.secho("Images successfully removed", fg='green')
+        click.secho("Images successfully removed", fg="green")
