64ccc81d335c4f50f738d4eb16a8e5d3aab52fba
Daven Quinn <dev@davenquinn.com>
2021-09-29 14:42:41 -0500

    WIP create-release command

--- _cli/sparrow_cli/commands/dev.py
+++ _cli/sparrow_cli/commands/dev.py
@@ -1,4 +1,4 @@
-from click import group, secho
+from click import group, secho, argument
 from os import environ, path, chdir
 from runpy import run_path
 from subprocess import run
@@ -27,12 +27,14 @@ def dev_reload():
     compose("exec frontend /app/node_modules/.bin/browser-sync reload")
 
 
-@sparrow_dev.command(name="sync-version-info")
-def sync_version_info():
+@sparrow_dev.command(name="create-release")
+@click.argument("version")
+def create_release(version):
     root_dir = environ.get("SPARROW_PATH")
     # We should bail here if we are running a bundled Sparrow...
     chdir(root_dir)
-    meta = run_path(path.join("backend", "sparrow", "meta.py"))
+    backend_meta = path.join("backend", "sparrow", "meta.py")
+    meta = run_path(backend_meta)
 
     version_file = "sparrow-version.json"
     info = json.load(open(version_file, "r"))
@@ -42,7 +44,7 @@ def sync_version_info():
     secho("Updating version information", fg="green", err=True)
     info["core"] = meta["__version__"]
     json.dump(info, open(version_file, "w"), indent=2)
-    run(["git", "add", "sparrow-version.json"])
+    run(["git", "add", backend_meta, "sparrow-version.json"])
 
 
 @sparrow_dev.command(name="clear-cache")
