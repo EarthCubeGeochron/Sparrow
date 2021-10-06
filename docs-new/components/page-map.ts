39036006b3cf5af63ae3463ff807b64653fe0e43
Casey Idzikowski <idzikoj1234@gmail.com>
2021-10-06 20:14:58 +0200

    getting started folder is more of a step-by-step guide to getting an instance running

--- docs-new/components/page-map.ts
+++ docs-new/components/page-map.ts
@@ -16,48 +16,37 @@ export type Links = Link[];
 // Links for the toolbar
 const navLinks: Links = [
   { href: "/about", label: "About" },
-  { href: "/docs", label: "Documentation" },
+  { href: "/docs/getting-started", label: "Documentation" },
   { href: "/guides", label: "Guides" },
-  { href: "/guides/get-started", label: "Get started!" },
+  { href: "/docs/getting-started", label: "Get started!" },
   h("li.spacer"),
-  h("li", null, h(DarkModeButton, { large: false })),
+  h("li", null, h(DarkModeButton, { large: false }))
 ];
 
 const guideLinks: Links = [
-  { href: "/guides/getting-started", label: "Getting started" },
   { href: "/guides/motivation-and-design", label: "Motivation and design" },
   {
     href: "/guides/schema",
-    label: "Schema and Importing data",
-    children: [
-      { href: "/guides/schema/importers", label: "Importers" },
-      {
-        href: "/guides/schema/wiscsims-example",
-        label: "WiscSims Importer Example",
-      },
-    ],
+    label: "Schema and importing data",
+    children: [{ href: "/guides/schema/importers", label: "Importers" }]
   },
   {
     href: "/guides/tutorials",
     label: "User Tutorials",
     children: [
-      {
-        href: "/guides/tutorials/metadata-importer",
-        label: "Metadata Importer"
-      },
       { href: "/guides/tutorials/datascience", label: "Data science" },
-      { href: "/guides/tutorials/views-and-tables", label: "Views and Tables" },
+      { href: "/guides/tutorials/views-and-tables", label: "Views and tables" },
       {
         href: "/guides/tutorials/frontend-visualizations",
-        label: "Frontend Visualization",
+        label: "Frontend visualization"
       },
       {
         href: "/guides/tutorials/custom-commands",
-        label: "Adding Custom Commands",
+        label: "Adding custom commands"
       },
-      { href: "/guides/tutorials/map-views", label: "Map Views" },
-      { href: "/guides/tutorials/docker-ubuntu", label: "Docker for Ubuntu" },
-    ],
+      { href: "/guides/tutorials/map-views", label: "Map views" },
+      { href: "/guides/tutorials/docker-ubuntu", label: "Docker for ubuntu" }
+    ]
   },
   { href: "/guides/embargo", label: "Embargo" },
   {
@@ -66,13 +55,13 @@ const guideLinks: Links = [
     children: [
       {
         href: "/guides/integrations/postgresql-client",
-        label: "PostgreSQL Client",
+        label: "PostgreSQL client"
       },
       {
         href: "/guides/integrations/qgis",
-        label: "QGIS",
-      },
-    ],
+        label: "QGIS"
+      }
+    ]
   },
   {
     href: "/guides/resources",
@@ -81,22 +70,36 @@ const guideLinks: Links = [
       { href: "/guides/resources/presentations", label: "Presentations" },
       {
         href: "/guides/resources/lab-implementations",
-        label: "Lab Implementations",
-      },
-    ],
-  },
+        label: "Lab implementations"
+      }
+    ]
+  }
 ];
 
 const aboutLinks: Links = [
   { href: "/about", label: "Motivation" },
   { href: "/about/implementations", label: "Implementations" },
-  { href: "/about/presentations", label: "Presentations" },
-  // { href: '/about/features', label: "Features"}
+  { href: "/about/presentations", label: "Presentations" }
 ];
 
 const docsLinks: Links = [
-  { href: "/docs", label: "Getting started" },
-  { href: "/docs/windows", label: "Sparrow on Windows" },
+  {
+    href: "/docs/getting-started",
+    label: "Getting started",
+    children: [
+      { href: "/docs/getting-started/create-lab", label: "Create a lab" },
+      { href: "/docs/getting-started/plugins", label: "Plugins" },
+      { href: "/docs/getting-started/importers", label: "Importers" },
+      {
+        href: "/docs/getting-started/metadata-importer",
+        label: "Metadata importer"
+      },
+      {
+        href: "/docs/getting-started/wiscsims-example",
+        label: "WiscSims importer example"
+      }
+    ]
+  },
   {
     href: "/docs/core",
     label: "Core application",
@@ -104,46 +107,45 @@ const docsLinks: Links = [
       { href: "/docs/core/cli", label: "Command-line interface" },
       { href: "/docs/core/server-config", label: "Server configuration" },
       { href: "/docs/core/plugins", label: "Plugins" },
-      { href: "/docs/core/extensions", label: "Extensions" },
-    ],
+      { href: "/docs/core/extensions", label: "Extensions" }
+    ]
   },
   { href: "/docs/database", label: "Database" },
   {
     href: "/docs/development/frontend",
     label: "Frontend",
     children: [
-      { href: "/docs/frontend/data-input-form", label: "Data Input Form" },
-      { href: "/docs/frontend/frames", label: "Extensions and Frames" },
-    ],
+      { href: "/docs/frontend/data-input-form", label: "Data input form" },
+      { href: "/docs/frontend/frames", label: "Extensions and frames" }
+    ]
   },
   {
     href: "/docs/development",
     label: "Development",
     children: [
       { href: "/docs/development/sparrow-cli", label: "Sparrow CLI" },
-      { href: "/docs/development/backend-plugins", label: "Backend Plugins" },
-      { href: "/docs/development/frontend-plugins", label: "Frontend Plugins" },
       {
         href: "/docs/development/local-development",
-        label: "Local development",
-      },
-    ],
+        label: "Local development"
+      }
+    ]
   },
+  { href: "/docs/windows", label: "Sparrow on windows" },
   {
     href: "/docs/misscellaneous",
-    label: "Misscellaneous",
+    label: "Miscellaneous",
     children: [
       {
         href: "/docs/misscellaneous/linux-permissions",
-        label: "Linux Permissions",
+        label: "Linux permissions"
       },
-      { href: "/docs/misscellaneous/docker-issues", label: "Docker Issues" },
+      { href: "/docs/misscellaneous/docker-issues", label: "Docker issues" },
       {
         href: "/docs/misscellaneous/trouble-shooting",
-        label: "Trouble Shooting",
-      },
-    ],
-  },
+        label: "Trouble shooting"
+      }
+    ]
+  }
 ];
 
 export { navLinks, aboutLinks, docsLinks, guideLinks };
