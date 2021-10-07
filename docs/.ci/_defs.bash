68c322a2d7031e2454d64253b19a12623dc4300a
Daven Quinn <dev@davenquinn.com>
2021-10-07 12:13:01 -0500

    Documentation container builds

--- docs/.ci/_defs.bash
+++ docs/.ci/_defs.bash
@@ -1,4 +1,4 @@
 export PKG_ROOT="ghcr.io/earthcubegeochron/sparrow"
 export tag="latest"
-export main_img="$PKG_ROOT/sparrow-documentation-site:$tag"
+export main_img="$PKG_ROOT/documentation-site:$tag"
 #export test_img="$PKG_ROOT/sparrow-documentation-test:$tag"
