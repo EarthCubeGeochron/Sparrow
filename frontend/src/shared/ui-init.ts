66807068c7d146dcef77041c59b3ca46d7c0f152
Daven Quinn <dev@davenquinn.com>
2022-09-20 18:57:51 -0500

    Big update to frontend packages

--- frontend/src/shared/ui-init.ts
+++ frontend/src/shared/ui-init.ts
@@ -2,7 +2,7 @@
 import "./ui-main.styl";
 //import "core-js/stable";
 import "regenerator-runtime";
-import "@macrostrat/ui-components/lib/esm/index.css";
+import "@macrostrat/ui-components/dist/ui-components.css";
 import { FocusStyleManager } from "@blueprintjs/core";
 import "@blueprintjs/core/lib/css/blueprint.css";
 
