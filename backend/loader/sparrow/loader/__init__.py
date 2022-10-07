866cbb7d8d250eaf489d2f4b283c0f4fadb91cf3
Daven Quinn <dev@davenquinn.com>
2022-10-07 08:13:23 -0600

    Updated sparrow loader again

--- backend/loader/sparrow/loader/__init__.py
+++ backend/loader/sparrow/loader/__init__.py
@@ -41,18 +41,18 @@ def show_loader_schemas(*schemas: List[str], nest_depth=0, show_dump_only=False)
     if len(schemas) == 0:
         schemas = coll.keys()
 
-    schemas = list(schemas)
-    schemas.sort()
-    prepended = [
-        "project",
-        "sample",
-        "session",
-        "analysis",
-        "datum",
-        "datum_type",
-    ]
-    schemas = [s for s in schemas if s not in prepended]
-    schemas = prepended + schemas
+        schemas = list(schemas)
+        schemas.sort()
+        prepended = [
+            "project",
+            "sample",
+            "session",
+            "analysis",
+            "datum",
+            "datum_type",
+        ]
+        schemas = [s for s in schemas if s not in prepended]
+        schemas = prepended + schemas
 
     for name in schemas:
         _schema = getattr(coll, name)
