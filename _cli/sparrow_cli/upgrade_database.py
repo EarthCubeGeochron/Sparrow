7135ebbc95199792d3cccb1cc1f327240489b381
Daven Quinn <dev@davenquinn.com>
2023-04-11 14:54:26 -0500

    Improve SQL management

--- _cli/sparrow_cli/upgrade_database.py
+++ _cli/sparrow_cli/upgrade_database.py
@@ -1,31 +1,11 @@
 """
 Scripts for upgrading the database cluster if it is out of date.
 """
-from contextlib import contextmanager
-from os import environ
-from pathlib import Path
-from .util.exceptions import SparrowCommandError
-from .util.shell import compose, cmd
-from rich import print
-from rich.console import Console
-import docker
-from docker.models.containers import Container
-import time
-from datetime import datetime
-import subprocess
-from subprocess import CalledProcessError
-import asyncio
-import typing as T
-import sys
 from macrostrat.utils import get_logger
-from docker.client import DockerClient
-
 from macrostrat.dinosaur import upgrade_cluster
 
 log = get_logger(__name__)
 
-
-console = Console()
 
 version_images = {11: "mdillon/postgis:11", 14: "postgis/postgis:14-3.3"}
 
