974c8a9055c3dd9cb756ea4a7b0ae1af9d8b58bb
Daven Quinn <dev@davenquinn.com>
2023-09-09 14:58:00 -0500

    Improve logging of public schema

--- backend/core_plugins/versioning/start-logging.sql
+++ backend/core_plugins/versioning/start-logging.sql
@@ -1,16 +1,32 @@
 -- Create event trigger to log schema changes ...
 SELECT pgmemento.create_schema_event_trigger(TRUE);
 
--- Start auditing for tables in `public` schema
 SELECT pgmemento.init(
   'public',
   'pgmemento_audit_id',
   -- Log existing data as imported
-  FALSE,
+  TRUE,
   -- Log new data
   FALSE,
+  -- Log "state" (not sure what this is)
   FALSE,
+  -- Trigger create tables (add on new tables...)
+  FALSE,
+  -- Tables to ignore
+  ARRAY['spatial_ref_sys']
+);
+
+-- Reinit to capture changes to audit approach.
+-- Start auditing for tables in `public` schema
+SELECT pgmemento.reinit(
+  'public',
+  'pgmemento_audit_id',
+  -- Log existing data as imported
   TRUE,
+  -- Log new data
+  FALSE,
+  -- Trigger on table creation
+  FALSE,
   -- Tables to ignore
   ARRAY['spatial_ref_sys']
 );
