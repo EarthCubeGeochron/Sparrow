d3610668019d9b670f3f0f5a9680b5ac8da6c37b
Daven Quinn <code@davenquinn.com>
2025-10-10 14:51:22 -0500

    Partial updates to image

--- backend/sparrow/core/startup/__init__.py
+++ backend/sparrow/core/startup/__init__.py
@@ -1,13 +1,16 @@
 from sparrow.logs import get_logger
 from sqlalchemy import create_engine
+from macrostrat.database.utils import run_query
 
 log = get_logger(__name__)
 
 
 def tables_exist(db_conn):
+
     engine = create_engine(db_conn)
     conn = engine.connect()
-    res = conn.execute(
-        "SELECT EXISTS (SELECT * FROM information_schema.tables WHERE table_schema = 'public' AND table_name = 'datum')"
+    res = run_query(
+        conn,
+        "SELECT EXISTS (SELECT * FROM information_schema.tables WHERE table_schema = 'public' AND table_name = 'datum')",
     ).scalar()
     return res
